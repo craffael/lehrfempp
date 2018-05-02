c6453041c6bdd2071c60644aa3d7d8ffcfd357db
craffael <raffael@casagrande.ch>
2018-05-02 22:06:49 +0200

    bugfix.

--- compile_test.sh
+++ compile_test.sh
@@ -16,12 +16,11 @@ mkdir -p ${DEPS_DIR} && cd ${DEPS_DIR}
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]] && [ ! -d "cmake" ]; then
   CMAKE_URL="https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.tar.gz"
   mkdir cmake && wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-  export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 elif [[ "${TRAVIS_OS_NAME}" == "osx" ]] && [ ! -d "cmake" ]; then
   CMAKE_URL="https://cmake.org/files/v3.11/cmake-3.11.1-Darwin-x86_64.tar.gz"
   mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=3 -xz -C cmake
-  export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 fi
+export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 
 # compile
 cd ${TRAVIS_BUILD_DIR}
