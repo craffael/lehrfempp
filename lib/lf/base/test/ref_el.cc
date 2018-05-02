9546fb96efa558caa877337b28bd4411cb41b0a0
craffael <raffael@casagrande.ch>
2018-05-02 22:20:00 +0200

    fix artificial error in ref_el.cc

--- lib/lf/base/test/ref_el.cc
+++ lib/lf/base/test/ref_el.cc
@@ -11,5 +11,5 @@ TEST(RefEl, dimensionCorrect)
   EXPECT_EQ(RefEl::kPoint.Dimension(), 0);
   EXPECT_EQ(RefEl::kSegment.Dimension(), 1);
   EXPECT_EQ(RefEl::kTria.Dimension(), 2);
-  EXPECT_EQ(RefEl::kQuad.Dimension(), 3);
+  EXPECT_EQ(RefEl::kQuad.Dimension(), 2);
 }
