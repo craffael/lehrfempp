ae1908f937e6a186c4726a1e60a111b633ec3962
craffael <raffael@casagrande.ch>
2018-05-02 21:43:36 +0200

    test...

--- compile_test.sh
+++ compile_test.sh
@@ -1,7 +1,7 @@
 # install cmake:
 mkdir -p ${HUNTER_ROOT}
 mkdir -p ${DEPS_DIR} && cd ${DEPS_DIR}
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]] && [ ! -d "cmake" ]; then
   CMAKE_URL="https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.tar.gz"
   mkdir cmake && wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
   export PATH=${DEPS_DIR}/cmake/bin:${PATH}
@@ -12,6 +12,7 @@ elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
 fi
 
 # compile
+cd ${TRAVIS_BUILD_DIR}
 export CXX=${COMPILER}
 cmake -H. -BBuild -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -Wdev
 cd Build
