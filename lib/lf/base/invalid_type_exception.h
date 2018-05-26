8ca9298575958a883f51d6331265be4e7a0d9c14
craffael <raffael@casagrande.ch>
2018-05-26 13:55:37 +0200

    bugfix for clang...?

--- lib/lf/base/invalid_type_exception.h
+++ lib/lf/base/invalid_type_exception.h
@@ -3,26 +3,27 @@
 
 #include <exception>
 #include <utility>
+#include <string>
 
 namespace lf::base {
 
-  /**
-   * @brief Thrown to signal that an argument passed to a function had the
-   * wrong (polymorphic) type.
-   */
-  class InvalidTypeException : public std::exception {
-  private:
-    std::string what_;
-  public:
-    InvalidTypeException() = default;
+/**
+ * @brief Thrown to signal that an argument passed to a function had the
+ * wrong (polymorphic) type.
+ */
+class InvalidTypeException : public std::exception {
+ private:
+  std::string what_;
 
-    explicit InvalidTypeException(std::string message)
-      : what_(std::move(message)) {
-    }
+ public:
+  InvalidTypeException() = default;
 
-    InvalidTypeException(const InvalidTypeException& other) = default;
-  };
+  explicit InvalidTypeException(std::string message)
+      : what_(std::move(message)) {}
 
-}
+  InvalidTypeException(const InvalidTypeException& other) = default;
+};
 
-#endif // __b58a30690f684a2b9422606fd526d0f4
+}  // namespace lf::base
+
+#endif  // __b58a30690f684a2b9422606fd526d0f4
