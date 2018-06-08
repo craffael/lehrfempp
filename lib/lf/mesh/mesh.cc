2639491defe7bc081e7fdb936bfe1717f954c18f
ralfh <ralfh@ethz.ch>
2018-06-08 21:50:32 +0200

    Implemented a simple index testing function in mesh.cc/mesh.h

---
+++ lib/lf/mesh/mesh.cc
@@ -0,0 +1,40 @@
+#include "mesh.h"
+#include <iostream>
+
+namespace lf::mesh
+{
+  bool testEntityIndexing(const Mesh &mesh) {
+    using size_type = Mesh::size_type; // type for indices
+    const size_type dim_mesh = mesh.DimMesh(); // dimension of meshed manifold
+    // Now run over all co-dimensions to check indexing
+    for (int co_dim = 0; co_dim <= dim_mesh; ++co_dim) {
+      // Number of entities of current co-dimension
+      const size_type no_of_entities = mesh.Size(co_dim); 
+      // counting array for occurrences of an index
+      std::vector<int> idx_use(no_of_entities,0);
+      // Traverse all entities of a given co-dimension
+      for(const Entity& e : mesh.Entities(co_dim)) {
+	// Fetch index of current entity
+	const size_type current_idx = mesh.Index(e);
+	// Check whether index is in range and, if so, count it
+	if (current_idx < no_of_entities) idx_use[current_idx]++;
+	else {
+	  std::cerr << "Index " << current_idx
+		    << " out of range" << std::endl;
+	  return false;
+	}
+      }
+      // Indices should occur only once. This means that the counting array
+      // should contain 1 in every component.
+      bool ok = true;
+      for (int idx_cnt = 0; idx_cnt < no_of_entities; ++idx_cnt) {
+	if (idx_use[idx_cnt] != 1) {
+	  std::cerr << "Index " << idx_cnt << " occurs "
+		    << idx_use[idx_cnt] << " times!" << std::endl;
+	  ok = false;
+	}
+      }
+      return ok;
+    } // end for
+  } // end testEntityIndexing()
+}
--- lib/lf/mesh/mesh.h
+++ lib/lf/mesh/mesh.h
@@ -1,9 +1,23 @@
-
+/** @file mesh.h
+ * @brief Header file for the mesh topology facilities of LehrFEM++
+ */
 #ifndef __7fd0772dc0d1474492ad46dc2b8cebbb
 #define __7fd0772dc0d1474492ad46dc2b8cebbb
 
 #include "entity.h"
 #include "mesh_builder.h"
 #include "mesh_interface.h"
+
+namespace lf::mesh
+{
+  /** 
+   * @brief Function for testing mesh indexing
+   * @param mesh A reference to the mesh to be checked
+   * @return False, if there is a problem with indexing
+   * This function tests whether all entities of a mesh are indexed 
+   * consecutively. 
+   */
+  bool testEntityIndexing(const Mesh &mesh);
+}
 
 #endif  // __7fd0772dc0d1474492ad46dc2b8cebbb
