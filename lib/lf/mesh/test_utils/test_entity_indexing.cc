98475c299c375ed1628d4aafa5ef32fa66db8802
craffael <raffael@casagrande.ch>
2018-06-09 11:16:09 +0200

    testEntityIndexing uses GoogleTest macros & hybrid2d tests use test_entity_indexing to test the indices...

--- lib/lf/mesh/test_utils/test_entity_indexing.cc
+++ lib/lf/mesh/test_utils/test_entity_indexing.cc
@@ -1,40 +1,31 @@
 #include <lf/mesh/mesh.h>
 #include <iostream>
+#include <gtest/gtest.h>
 
 namespace lf::mesh::test_utils
 {
-  bool testEntityIndexing(const Mesh &mesh) {
+  void testEntityIndexing(const Mesh &mesh) {
     using size_type = Mesh::size_type; // type for indices
-    const size_type dim_mesh = mesh.DimMesh(); // dimension of meshed manifold
+    const auto dim_mesh = mesh.DimMesh(); // dimension of meshed manifold
     // Now run over all co-dimensions to check indexing
-    for (int co_dim = 0; co_dim <= dim_mesh; ++co_dim) {
+    for (size_type co_dim = 0; co_dim <= dim_mesh; ++co_dim) {
       // Number of entities of current co-dimension
-      const size_type no_of_entities = mesh.Size(co_dim); 
+      const auto no_of_entities = mesh.Size(co_dim); 
       // counting array for occurrences of an index
       std::vector<int> idx_use(no_of_entities,0);
       // Traverse all entities of a given co-dimension
       for(const Entity& e : mesh.Entities(co_dim)) {
 	// Fetch index of current entity
-	const size_type current_idx = mesh.Index(e);
+	const auto current_idx = mesh.Index(e);
 	// Check whether index is in range and, if so, count it
-	if (current_idx < no_of_entities) idx_use[current_idx]++;
-	else {
-	  std::cerr << "Index " << current_idx
-		    << " out of range" << std::endl;
-	  return false;
-	}
+        EXPECT_LT(current_idx, no_of_entities) << "Index " << current_idx << " out of range";
+	idx_use[current_idx]++;
       }
       // Indices should occur only once. This means that the counting array
       // should contain 1 in every component.
-      bool ok = true;
-      for (int idx_cnt = 0; idx_cnt < no_of_entities; ++idx_cnt) {
-	if (idx_use[idx_cnt] != 1) {
-	  std::cerr << "Index " << idx_cnt << " occurs "
-		    << idx_use[idx_cnt] << " times!" << std::endl;
-	  ok = false;
-	}
+      for (size_type idx_cnt = 0; idx_cnt < no_of_entities; ++idx_cnt) {
+        EXPECT_EQ(idx_use[idx_cnt], 1) << "Index " << idx_cnt << " occurs " << idx_use[idx_cnt] << " times!";
       }
-      return ok;
     } // end for
   } // end testEntityIndexing()
 }
