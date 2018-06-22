1c7ae134343781c67ae1ae44ea09c0cce964a63b
craffael <raffael@casagrande.ch>
2018-06-22 19:40:38 +0200

    some bugfixes for travis.

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -7,26 +7,28 @@
 # - change into your CMake build directory and call this script from there.
 # - If you want to run it in parallel, append e.g. `-j4` as an argument.
 
+set -e
 
 # Check that compile_commands.json exists.
 if [[ ! -e ./compile_commands.json ]]; then
   echo "compile_commands.json not found in current directory"
   echo "run 'cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On . ' to create it."
+  exit 1
 fi
 
 # find clang-tidy
 if [ -x "$(command -v clang-tidy)" ]; then
-  ct=$(command -v clang-tidy)
+  ct=clang-tidy
 fi
 if [ -x "$(command -v clang-tidy-5.0)" ]; then
-  ct=$(command -v clang-tidy-5.0)
+  ct=clang-tidy-5.0
 fi
 if [ -x "$(command -v clang-tidy-6.0)" ]; then
-  ct=$(command -v clang-tidy-6.0)
+  ct=clang-tidy-6.0
 fi
 
 if [ -z "$ct" ]; then
   echo "clang-tidy, clang-tidy-6.0 or clang-tidy-5.0 not found in path"
 fi
-
-$(dirname $0)/travis/run-clang-tidy.py -p . -header-filter=lib/ '^((?!snippets|/test/|/test_utils/).)*$' $1
+echo $ct
+$(dirname $0)/travis/run-clang-tidy.py -p . -clang-tidy-binary $ct -header-filter=lib/ '^((?!snippets|/test/|/test_utils/).)*$' $1
