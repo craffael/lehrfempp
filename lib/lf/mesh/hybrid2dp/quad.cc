0c6431d30d016d9988a4627efe451a08589cb2ad
ralfh <ralfh@ethz.ch>
2018-07-02 07:29:38 +0200

    Added consistency checks to constructors of Triangle and Quadrilateral

--- lib/lf/mesh/hybrid2dp/quad.cc
+++ lib/lf/mesh/hybrid2dp/quad.cc
@@ -11,6 +11,62 @@
 #include "segment.h"
 
 namespace lf::mesh::hybrid2dp {
+  Quadrilateral::Quadrilateral(size_type index,
+			       std::unique_ptr<geometry::Geometry>&& geometry,
+			       const Point* corner0, const Point* corner1,
+			       const Point* corner2, const Point* corner3,
+			       const Segment* edge0, const Segment* edge1,
+			       const Segment* edge2, const Segment* edge3)
+      : index_(index),
+        geometry_(std::move(geometry)),
+        nodes_({corner0, corner1, corner2, corner3}),
+        edges_({edge0, edge1, edge2, edge3}) {
+    LF_VERIFY_MSG(corner0 != nullptr, "Invalid pointer to corner 0");
+    LF_VERIFY_MSG(corner1 != nullptr, "Invalid pointer to corner 1");
+    LF_VERIFY_MSG(corner2 != nullptr, "Invalid pointer to corner 2");
+    LF_VERIFY_MSG(corner3 != nullptr, "Invalid pointer to corner 3");
+    LF_VERIFY_MSG(edge0 != nullptr, "Invalid pointer to edge 0");
+    LF_VERIFY_MSG(edge1 != nullptr, "Invalid pointer to edge 1");
+    LF_VERIFY_MSG(edge2 != nullptr, "Invalid pointer to edge 2");
+    LF_VERIFY_MSG(edge3 != nullptr, "Invalid pointer to edge 3");
+    if (geometry_) {
+      LF_VERIFY_MSG(geometry_->DimLocal() == 2,
+                    "Geometry must describe a 2D cell");
+      LF_VERIFY_MSG(geometry_->RefEl() == base::RefEl::kQuad(),
+                    "Cell geometry must fit a quad");
+    }
+    
+    // Consistency check: make sure that sub-entities of sub-entities
+    // agree with sub-sub-entities
+
+    const lf::base::RefEl ref_el_quad(lf::base::RefElType::kQuad);
+    // Run through all edges
+    for (int ed_loc_idx = 0; ed_loc_idx < 4; ed_loc_idx++) {
+      // Pointer to current edge
+      const Segment *ed_ptr = edges_.at(ed_loc_idx);
+      // Check for segement type
+      LF_VERIFY_MSG(ed_ptr->RefEl() == lf::base::RefEl::kSegment(),
+		    "Wrong type for edge " << ed_loc_idx);
+      // Fetch nodes from current edge
+      base::RandomAccessRange<const Entity> ed_nodes(ed_ptr->SubEntities(1));
+      
+      // Obtain local indices of endpoints of edge
+      const size_type loc_idx_p0 =
+	ref_el_quad.SubSubEntity2SubEntity(1,ed_loc_idx,1,0);
+      const size_type loc_idx_p1 =
+	ref_el_quad.SubSubEntity2SubEntity(1,ed_loc_idx,1,1);
+      const Point *p0_ptr = nodes_.at(loc_idx_p0);
+      const Point *p1_ptr = nodes_.at(loc_idx_p1);
+
+      // Verify that the nodes of the triangle are nodes of the edge as well
+      LF_VERIFY_MSG((ed_nodes[0] == *p0_ptr) || (ed_nodes[0] == *p1_ptr),
+		    "Node 0 of edge " << ed_loc_idx << " not a quad node");
+      LF_VERIFY_MSG((ed_nodes[1] == *p0_ptr) || (ed_nodes[1] == *p1_ptr),
+		    "Node 1 of edge " << ed_loc_idx << " not a quad node");
+    }
+  } // end constructor
+  
+  // Access to sub-entities
 base::RandomAccessRange<const mesh::Entity> Quadrilateral::SubEntities(
     char rel_codim) const {
   auto l = [&](auto i) -> const mesh::Entity& { return **i; };
