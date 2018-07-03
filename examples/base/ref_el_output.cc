deedf7365267ce5d425fff9ec2707fbc29fce369
Gina Magnussen <gina.magnussen@sam.math.ethz.ch>
2018-07-03 15:16:45 +0200

    Change in test output

--- examples/base/ref_el_output.cc
+++ examples/base/ref_el_output.cc
@@ -10,6 +10,8 @@ int main() {
 
   auto re_node = lf::base::RefEl::kPoint();
   std::cout << "Information about node: " << re_node << std::endl;
+  int dim_seg = re_seg.Dimension();
+  std::cout << "Dimension of segment: " << dim_seg << std::endl;
 
   auto re_seg = lf::base::RefEl::kSegment();
   std::cout << "Information about segment: " << re_seg << std::endl;
