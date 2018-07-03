32505b6333a3a65207cfef0dcfd81e2f48737379
ralfh <ralfh@ethz.ch>
2018-07-03 06:32:25 +0200

    Further development of refinement facilities

--- lib/lf/geometry/segment_o1.h
+++ lib/lf/geometry/segment_o1.h
@@ -5,6 +5,9 @@
 
 namespace lf::geometry {
 
+  /**
+   * @brief A straight edge defined by the location of its two endpoints
+   */
 class SegmentO1 : public Geometry {
  public:
   explicit SegmentO1(Eigen::Matrix<double, Eigen::Dynamic, 2> coords)
@@ -20,6 +23,17 @@ class SegmentO1 : public Geometry {
   Eigen::VectorXd IntegrationElement(
       const Eigen::MatrixXd& local) const override;
   std::unique_ptr<Geometry> SubGeometry(dim_t codim, dim_t i) const override;
+
+  /** @brief creation of child geometry for the sake of mesh refinement 
+   *
+   * @see Geometry::ChildGeometry()
+   * @see RefinementPattern
+   * @param ref_pattern three refinement patterns are supported
+   * - rp_copy: just copies the geometry information of the segment
+   * - rp_split, rp_regular: split edge in the middle.
+   */
+  std::unique_ptr<Geometry>
+  ChildGeometry(int ref_pattern,int selector) const override;
 
  private:
   Eigen::Matrix<double, Eigen::Dynamic, 2> coords_;
