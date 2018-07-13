67d7ef637d66a55e26764b7c89273509bf45778e
Gina Magnussen <gina.magnussen@sam.math.ethz.ch>
2018-07-13 16:23:08 +0200

    Output for Geometry and Entity not working

--- examples/geometry/geometry_output.cc
+++ examples/geometry/geometry_output.cc
@@ -4,11 +4,15 @@
 
 #include <iostream>
 #include "lf/geometry/geometry.h"
+// #include "lf/base/base.h"
 
 int main() {
   std::cout << "Output of information of geometry" << std::endl;
   //auto geom = lf::geometry::Point; // Not working
   //std::cout << "Geometry object: " << geom << std::cout;
+
+  // auto geom = lf::geometry::Compose()
+
 
   return 0L;
 }
--- examples/mesh/mesh_entity_output.cc
+++ examples/mesh/mesh_entity_output.cc
@@ -3,13 +3,18 @@
  */
 
 #include <iostream>
+
 #include "lf/mesh/hybrid2d/entity.h"
+#include "lf/mesh/hybrid2dp/triangle.h"
+#include "lf/mesh/mesh_factory.h"
 #include "lf/mesh/entity.h"
+#include "lf/base/base.h"
 
 int main() {
   std::cout << "Output of information for mesh entity elements" << std::endl;
 
   //auto entity = lf::mesh::hybrid2d::Entity(); // Not working
+  auto test = lf::mesh::hybrid2dp::Triangle::RefEl();
   //std::cout << "Print of entity: " << entity << std::endl;
 
   return 0L;
