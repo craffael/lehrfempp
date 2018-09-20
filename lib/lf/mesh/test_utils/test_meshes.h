d546eb3705d65b6b5a16d859bc58295226bfb11e
ralfh <ralfh@ethz.ch>
2018-09-20 07:40:17 +0200

    Documented GenerateHybrid2DTestMesh()

--- lib/lf/mesh/test_utils/test_meshes.h
+++ lib/lf/mesh/test_utils/test_meshes.h
@@ -8,11 +8,22 @@
 #include <lf/mesh/hybrid2dp/hybrid2dp.h>
 #include <lf/mesh/mesh.h>
 
+/**
+ * @brief Utilities for testing sanity of mesh data structures and
+ *        tests involving meshes
+ */
 namespace lf::mesh::test_utils {
 /**
  * @brief Generates a simple 2D hybrid test mesh
+ *
+ * The following line of code provides a pointer to the test mesh:
+ * ~~~
+ *   #include "lf/mesh/test_utils/test_meshes.h"
+ *   ...
+ *   auto mesh_p = lf::mesh::test_utils::GenerateHybrid2DTestMesh();
+ * ~~~
+ * @image html testmesh.png
  */
-
 std::shared_ptr<lf::mesh::Mesh> GenerateHybrid2DTestMesh();
 
 }  // namespace lf::mesh::test_utils
