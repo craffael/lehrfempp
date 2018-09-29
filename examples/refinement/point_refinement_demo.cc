4aa5053d60e79897cd0b74a0e09a54f5ff17a602
Anian Ruoss <anian.ruoss@gmail.com>
2018-09-29 15:19:40 +0200

    Access cell coordinates via NodeCoords()

--- examples/refinement/point_refinement_demo.cc
+++ examples/refinement/point_refinement_demo.cc
@@ -27,12 +27,12 @@ int main() {
     using lf::mesh::utils::TikzOutputCtrl;
 
     std::shared_ptr<lf::mesh::hybrid2dp::MeshFactory> mesh_factory_ptr =
-        std::make_shared<lf::mesh::hybrid2dp::MeshFactory>(2);
+            std::make_shared<lf::mesh::hybrid2dp::MeshFactory>(2);
 
     // Build single-cell tensor product mesh on unit square
     lf::mesh::hybrid2d::TPQuadMeshBuilder builder(mesh_factory_ptr);
-    builder.setBottomLeftCorner(Eigen::Vector2d{0,0});
-    builder.setTopRightCorner(Eigen::Vector2d{1,1});
+    builder.setBottomLeftCorner(Eigen::Vector2d{0, 0});
+    builder.setTopRightCorner(Eigen::Vector2d{1, 1});
     builder.setNoXCells(1);
     builder.setNoYCells(1);
     std::shared_ptr<lf::mesh::Mesh> mesh_ptr = builder.Build();
@@ -45,55 +45,61 @@ int main() {
     // Build mesh hierarchy
     lf::refinement::MeshHierarchy multi_mesh(mesh_ptr, mesh_factory_ptr);
 
-    // Mark cell containing point (0.5, 0.5) which should be refined
-    auto marker = [] (const lf::mesh::Mesh &mesh, const lf::mesh::Entity &edge) {
-        Eigen::MatrixXd ref_a(2, 1);
-        Eigen::MatrixXd ref_b(2, 1);
-        Eigen::MatrixXd ref_c(2, 1);
-        Eigen::MatrixXd ref_d(2, 1);
-        ref_a << 0,0;
-        ref_b << 1,0;
-        ref_c << 1,1;
-        ref_d << 0,1;
+    // Mark cells containing point (0.5, 0.5) which should be refined
+    Eigen::MatrixXd point(2, 1);
+    point << .5, .5;
 
-        Eigen::MatrixXd point(2,1);
-        point << .5,.5;
+    auto marker = [&point](const lf::mesh::Mesh &mesh,
+                           const lf::mesh::Entity &edge) {
 
         for (const lf::mesh::Entity &cell : mesh.Entities(0)) {
-            auto geom = cell.Geometry();
+            const lf::geometry::Geometry *geom_ptr = cell.Geometry();
+            lf::base::RefEl ref_el = cell.RefEl();
+            const Eigen::MatrixXd &ref_el_coords(ref_el.NodeCoords());
+            const Eigen::MatrixXd glob_coords(geom_ptr->Global(ref_el_coords));
 
-            // Due to refinement we have to make sure that the global
-            // coordinates are always ordered in the same way:
+            // Cells adjacent to (0.5, 0.5) have to be quadrilaterals
+            if (glob_coords.cols() < 4) {
+                continue;
+            }
+
+            // As the refinement orders coordinates randomly we have to make
+            // sure that they are always ordered in the same way to check
+            // whether a point lies inside the cell or not:
             // c ---- d
             //   |  |
             // a ---- b 
-            std::vector<Eigen::MatrixXd> glob_coords = {geom->Global(ref_a), 
-                geom->Global(ref_b), geom->Global(ref_c), geom->Global(ref_d)};
+            std::vector<Eigen::MatrixXd> coord_vec = {glob_coords.col(0),
+                                                      glob_coords.col(1),
+                                                      glob_coords.col(2),
+                                                      glob_coords.col(3)};
 
-            auto coord_sort = [] (Eigen::MatrixXd x, Eigen::MatrixXd y) {
-                return (x(1,0) < y(1,0)) || 
-                       ((x(1,0) == y(1,0)) && (x(0,0) <= y(0,0)));
+            auto coord_sort = [](Eigen::MatrixXd x, Eigen::MatrixXd y) {
+                return (x(1, 0) < y(1, 0)) ||
+                       ((x(1, 0) == y(1, 0)) && (x(0, 0) <= y(0, 0)));
             };
-            std::sort(glob_coords.begin(), glob_coords.end(), coord_sort);
+            std::sort(coord_vec.begin(), coord_vec.end(), coord_sort);
 
-            Eigen::MatrixXd glob_a = glob_coords[0];
-            Eigen::MatrixXd glob_b = glob_coords[1];
-            Eigen::MatrixXd glob_c = glob_coords[2];
-            Eigen::MatrixXd glob_d = glob_coords[3];
+            Eigen::MatrixXd glob_a = coord_vec[0];
+            Eigen::MatrixXd glob_b = coord_vec[1];
+            Eigen::MatrixXd glob_c = coord_vec[2];
+            Eigen::MatrixXd glob_d = coord_vec[3];
 
             // Check if point lies inside cell
-            if (glob_a(0,0) <= point(0,0) && glob_a(1,0) <= point(1,0) &&
-                glob_b(0,0) >= point(0,0) && glob_b(1,0) <= point(1,0) &&
-                glob_c(0,0) <= point(0,0) && glob_c(1,0) >= point(1,0) &&
-                glob_d(0,0) >= point(0,0) && glob_d(1,0) >= point(1,0)) {
+            if (glob_a(0, 0) <= point(0, 0) && glob_a(1, 0) <= point(1, 0) &&
+                glob_b(0, 0) >= point(0, 0) && glob_b(1, 0) <= point(1, 0) &&
+                glob_c(0, 0) <= point(0, 0) && glob_c(1, 0) >= point(1, 0) &&
+                glob_d(0, 0) >= point(0, 0) && glob_d(1, 0) >= point(1, 0)) {
 
                 // Check if cell contains edge
                 auto edges = cell.SubEntities(1);
-                if (edges.end() != std::find(edges.begin(),edges.end(),edge)) {
+                if (edges.end() !=
+                    std::find(edges.begin(), edges.end(), edge)) {
                     return true;
                 }
             }
         }
+
         return false;
     };
 
@@ -110,19 +116,19 @@ int main() {
         // Obtain pointer to mesh on finest level
         const size_type n_levels = multi_mesh.NumLevels();
         std::shared_ptr<const lf::mesh::Mesh> mesh_fine =
-            multi_mesh.getMesh(n_levels - 1);
+                multi_mesh.getMesh(n_levels - 1);
 
         // Print number of entities of various co-dimensions
-        std::cout << "Mesh on level " << n_levels - 1 << ": " 
-            << mesh_fine->Size(2)<< " nodes, " 
-            << mesh_fine->Size(1) << " edges, " 
-            << mesh_fine->Size(0) << " cells," << std::endl;
+        std::cout << "Mesh on level " << n_levels - 1 << ": "
+                  << mesh_fine->Size(2) << " nodes, "
+                  << mesh_fine->Size(1) << " edges, "
+                  << mesh_fine->Size(0) << " cells," << std::endl;
 
         lf::mesh::utils::writeTikZ(
-                *mesh_fine, 
+                *mesh_fine,
                 std::string("refinement_mesh") + std::to_string(step) + ".txt",
                 TikzOutputCtrl::RenderCells | TikzOutputCtrl::CellNumbering |
-                TikzOutputCtrl::VerticeNumbering | 
+                TikzOutputCtrl::VerticeNumbering |
                 TikzOutputCtrl::NodeNumbering | TikzOutputCtrl::EdgeNumbering);
 
         if (point_refinement) {
