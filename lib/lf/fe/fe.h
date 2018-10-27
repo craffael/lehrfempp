1dd779b1b5cf266f36a56f4699ef56635bff7bf8
ralfh <ralfh@ethz.ch>
2018-10-27 18:49:08 +0200

    Added header fe.h

---
+++ lib/lf/fe/fe.h
@@ -0,0 +1,23 @@
+#ifndef LF_FE_H
+#define LF_FE_H
+/***************************************************************************
+ * LehrFEM++ - A simple C++ finite element libray for teaching
+ * Developed from 2018 at the Seminar of Applied Mathematics of ETH Zurich,
+ * lead developers Dr. R. Casagrande and Prof. R. Hiptmair
+ ***************************************************************************/
+
+/**
+ * @file
+ * @brief Finite elements: local definition and assembly
+ * elliptic BVPs
+ * @author Ralf Hiptmair
+ * @date October 2018
+ * @copyright MIT License
+ */
+
+#include "lagr_fe.h"
+#include "loc_comp_ellbvp.h"
+
+namespace lf::fe {}  // namespace lf::fe
+
+#endif
