fe54fa946b622b2949075c91af138d1ea6ae44e6
ralfh <ralfh@ethz.ch>
2018-10-30 08:09:54 +0100

    Small fix

--- lib/lf/assemble/test/assembly_tests.cc
+++ lib/lf/assemble/test/assembly_tests.cc
@@ -1,4 +1,4 @@
-/* **************************************************************************
+/***************************************************************************
  * LehrFEM++ - A simple C++ finite element libray for teaching
  * Developed from 2018 at the Seminar of Applied Mathematics of ETH Zurich,
  * lead developers Dr. R. Casagrande and Prof. R. Hiptmair
