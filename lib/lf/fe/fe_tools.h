362bf8ee28da17aaa65e1d32308ebce03bb08c8f
ralfh <ralfh@ethz.ch>
2018-11-18 13:25:01 +0100

    FE facilities: interpolation and norms

---
+++ lib/lf/fe/fe_tools.h
@@ -0,0 +1,242 @@
+#ifndef LF_FETOOLS_H
+#define LF_FETOOLS_H
+/***************************************************************************
+ * LehrFEM++ - A simple C++ finite element libray for teaching
+ * Developed from 2018 at the Seminar of Applied Mathematics of ETH Zurich,
+ * lead developers Dr. R. Casagrande and Prof. R. Hiptmair
+ ***************************************************************************/
+
+/**
+ * @file
+ * @brief Functions acting on finite element spaces
+ * @author Ralf Hiptmair
+ * @date November 2018
+ * @copyright MIT License
+ */
+
+#include <lf/assemble/assemble.h>
+#include <lf/quad/quad.h>
+#include "fe_space.h"
+#include "loc_comp_norms.h"
+
+namespace lf::fe {
+
+CONTROLDECLAREINFO(ctrl_l2, "ctrl_l2", "Output control for SumCellFEContrib()");
+static const unsigned int kout_l2_qr = 1;
+static const unsigned int kout_l2_rsfvals = 2;
+
+/**
+ * @brief Summation of cell values computed based on a finite element
+ *        function specified through its basis expansion coefficients
+ *
+ * @tparam LOC_COMP type taking care of local computations
+ * @tparam COEFFVECTOR a vanilla vector type
+ * @tparam SELECTOR a predicate for selecting cells
+ *
+ * ### type requirements
+ *
+ * - The type LOC_COMP must feature an `isActive()` method for the
+ *   selection of cells to be visited, must provide a type `dofvector_t`
+ *   for passing coefficients of local shape functions and a method
+ * ~~~
+ * double operator ()(const lf::mesh::Entity &cell, const DOFVECTOR &dofs);
+ * ~~~
+ *   that performs the local computations.
+ * - The type COEFFVECTOR must provide component access through `[]`,
+ *   a `size()` method telling the vector length, and `value_type` typedef
+ *   telling the  component type.
+ * - The type functor must provide an evaluation operator `()` taking
+ *   a point coordinate vector `Eigen::Vector2d` as an argument.
+ *
+ * @param dofh Local-to-global index mapping belonging to a finite element space
+ * @param loc_comp reference to helper object for local computations
+ *        This object must be aware of the shape functions!
+ * @param uh coefficient vector of finite element function
+ *
+ */
+template <typename LOC_COMP, typename COEFFVECTOR, typename SELECTOR>
+double SumCellFEContrib(const lf::assemble::DofHandler &dofh,
+                        LOC_COMP &loc_comp, const COEFFVECTOR &uh,
+                        SELECTOR &&pred) {
+  // Type for passing local coefficient vectors
+  using dofvector_t = std::vector<typename COEFFVECTOR::value_type>;
+  // Retrieve the mesh
+  const lf::mesh::Mesh &mesh{*dofh.Mesh()};
+
+  // Check whether sufficiently large vector uh
+  LF_ASSERT_MSG(uh.size() >= dofh.NoDofs(), "uh vector too short!");
+
+  double sum = 0.0;
+  /// loop over cells ( = codim-0 entities)
+  for (const lf::mesh::Entity &cell : mesh.Entities(0)) {
+    if (pred(cell)) {
+      // Number of local shape functions
+      const size_type num_loc_dofs = dofh.NoLocalDofs(cell);
+      // Allocate a temporary vector of appropriate size
+      dofvector_t loc_coeffs(num_loc_dofs);
+      // Fetch global numbers of local shape functions
+      lf::base::RandomAccessRange<const gdof_idx_t> ldof_gidx(
+          dofh.GlobalDofIndices(cell));
+      // Copy degrees of freedom into temporory vector
+      for (int j = 0; j < num_loc_dofs; ++j) {
+        loc_coeffs[j] = uh[ldof_gidx[j]];
+      }
+      // Sum local contribution
+      sum += loc_comp(cell, loc_coeffs);
+    }  // end if (isACtive())
+  }
+  return sum;
+}
+
+/** @brief predicate for non-discriminatory positive selection of entities */
+struct DefaultEntitySelector {
+  bool operator()(const lf::mesh::Entity & /*cell*/) { return true; }
+};
+
+/**
+ * @brief Summation of local contributions over _all_ cells
+ *
+ * @sa SumCellFEContrib(const lf::assemble::DofHandler,LOC_COMP, const
+ * COEFFVECTOR &uh, SELECTOR)
+ */
+
+template <typename LOC_COMP, typename COEFFVECTOR>
+double SumCellFEContrib(const lf::assemble::DofHandler &dofh,
+                        LOC_COMP &loc_comp, const COEFFVECTOR &uh) {
+  return SumCellFEContrib(dofh, loc_comp, uh, DefaultEntitySelector());
+}
+
+/**
+ * @brief Computation of an inner product norm of the difference of a finite
+ * element function and a general functions.
+ *
+ * @tparam LOC_NORM_COMP helper type like lf::fe::LocalL2NormDifference
+ * @tparam COEFFVECTOR a vanilla vector type
+ * @tparam SELECTOR a predicate for selecting cells
+ *
+ * ### type requirements
+ *
+ * - The type LOC_NORM_COMP must feature an `isActive()` method for the
+ *   selection of cells to be visited, must provide a type `dofvector_t`
+ *   for passing coefficients of local shape functions and a method
+ * ~~~
+ * double operator () (const lf::mesh::Entity &cell, const DOFVECTOR &dofs);
+ * ~~~
+ *   that performs the local computations and returns the **square** of
+ *   the local norm of the difference function
+ * - The type COEFFVECTOR must provide component access through `[]`,
+ *   a `size()` method telling the vector length, and `value_type` typedef
+ *   telling the  component type.
+ * - The type functor must provide an evaluation operator `()` taking
+ *   a point coordinate vector `Eigen::Vector2d` as an argument.
+ *
+ * @param dofh Local-to-global index mapping belonging to a finite element space
+ * @param loc_comp reference to helper object for local computations
+ *        This object must be aware of the shape functions!
+ * @param uh coefficient vector of finite element function
+ *
+ */
+template <typename LOC_NORM_COMP, typename COEFFVECTOR, typename SELECTOR>
+double NormOfDifference(const lf::assemble::DofHandler &dofh,
+                        LOC_NORM_COMP &loc_comp, const COEFFVECTOR &uh,
+                        SELECTOR &&pred) {
+  const double norm_sq = SumCellFEContrib(dofh, loc_comp, uh, pred);
+  return std::sqrt(norm_sq);
+}
+template <typename LOC_NORM_COMP, typename COEFFVECTOR>
+double NormOfDifference(const lf::assemble::DofHandler &dofh,
+                        LOC_NORM_COMP &loc_comp, const COEFFVECTOR &uh) {
+  return NormOfDifference(dofh, loc_comp, uh, DefaultEntitySelector());
+}
+
+// ******************************************************************************
+
+// Output control for nodal projection
+CONTROLDECLAREINFO(ctrl_prj, "ctrl_prj",
+                   "Output control for  NodalProjection()");
+static const unsigned int kout_prj_cell = 1;
+static const unsigned int kout_prj_vals = 2;
+
+/*
+ * @brief Computes nodal projection of a function and returns the finite
+ * element basis expansion coefficients of the result
+ *
+ */
+template <typename FUNCTOR, typename SELECTOR>
+Eigen::Matrix<typename std::invoke_result<FUNCTOR, Eigen::VectorXd>::type,
+              Eigen::Dynamic, 1>
+NodalProjection(const UniformScalarFiniteElementSpace &fe_space, FUNCTOR u,
+                SELECTOR &&pred) {
+  // Scalar type determined by the function
+  using scalar_t = typename std::invoke_result<FUNCTOR, Eigen::VectorXd>::type;
+  // Return type, type for FE coefficient vector
+  using dof_vec_t = Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>;
+
+  // Underlying mesh instance
+  const lf::mesh::Mesh &mesh{*fe_space.Mesh()};
+  // Fetch local-to-global index mapping for shape functions
+  const lf::assemble::DofHandler &dofh{fe_space.LocGlobMap()};
+
+  // Vector for returning expansion coefficients
+  dof_vec_t glob_dofvec(dofh.NoDofs());
+  glob_dofvec.setZero();
+
+  // Loop over all cells
+  for (const lf::mesh::Entity &cell : mesh.Entities(0)) {
+    // Topological type of the cell
+    const lf::base::RefEl ref_el{cell.RefEl()};
+    // Query the shape of the cell
+    const lf::geometry::Geometry *geo_ptr = cell.Geometry();
+    LF_ASSERT_MSG(geo_ptr != nullptr, "Invalid geometry!");
+    LF_ASSERT_MSG((geo_ptr->DimLocal() == 2),
+                  "Only 2D implementation available!");
+    SWITCHEDSTATEMENT(ctrl_prj, kout_prj_cell,
+                      std::cout << ref_el << ", shape = \n"
+                                << geo_ptr->Global(ref_el.NodeCoords())
+                                << std::endl);
+
+    // Information about local shape functions on reference element
+    const ScalarReferenceFiniteElement<double> &ref_shape_fns{
+        fe_space.ShapeFunctionLayout(ref_el)};
+    // Number of evaluation nodes
+    const size_type num_eval_nodes = ref_shape_fns.NumEvaluationNodes();
+    // Obtain reference coordinates for evaluation nodes
+    const Eigen::MatrixXd ref_nodes(ref_shape_fns.EvaluationNodes());
+    // Obtain world coordinates of evaluation points
+    const Eigen::MatrixXd mapped_evlpts(geo_ptr->Global(ref_nodes));
+    // Collect values of function to be projected in a row vector
+    Eigen::Matrix<scalar_t, 1, Eigen::Dynamic> uvalvec(num_eval_nodes);
+    for (int j = 0; j < num_eval_nodes; ++j) {
+      uvalvec[j] = u(mapped_evlpts.col(j));
+    }
+    // Compute the resulting local degrees of freedom
+    auto dofvec(ref_shape_fns.NodalValuesToDofs(uvalvec));
+
+    // Set the corresponing global degrees of freedom
+    // Note: "Setting", not "adding to"
+    // Number of local shape functions
+    const size_type num_loc_dofs = dofh.NoLocalDofs(cell);
+    LF_ASSERT_MSG(
+        dofvec.size() == num_loc_dofs,
+        "Size mismatch: " << dofvec.size() << " <-> " << num_loc_dofs);
+    // Fetch global numbers of local shape functions
+    lf::base::RandomAccessRange<const gdof_idx_t> ldof_gidx(
+        dofh.GlobalDofIndices(cell));
+    // Insert dof values into the global coefficient vector
+    for (int j = 0; j < num_loc_dofs; ++j) {
+      glob_dofvec[ldof_gidx[j]] = dofvec[j];
+    }
+  }
+  return glob_dofvec;
+}
+
+template <typename FUNCTOR>
+Eigen::Matrix<typename std::invoke_result<FUNCTOR, Eigen::VectorXd>::type,
+              Eigen::Dynamic, 1>
+NodalProjection(const UniformScalarFiniteElementSpace &fe_space, FUNCTOR u) {
+  return NodalProjection(fe_space, u, DefaultEntitySelector());
+}
+
+}  // namespace lf::fe
+
+#endif
---
+++ lib/lf/fe/loc_comp_norms.h
@@ -0,0 +1,336 @@
+#ifndef LF_LOCCOMPNORMS_H
+#define LF_LOCCOMPNORMS_H
+/***************************************************************************
+ * LehrFEM++ - A simple C++ finite element libray for teaching
+ * Developed from 2018 at the Seminar of Applied Mathematics of ETH Zurich,
+ * lead developers Dr. R. Casagrande and Prof. R. Hiptmair
+ ***************************************************************************/
+
+/**
+ * @file
+ * @brief Classes for the local computation of norms of (differences of)
+ * functions elliptic BVPs
+ * @author Ralf Hiptmair
+ * @date November 2018
+ * @copyright MIT License
+ */
+
+#include <cmath>
+#include <iostream>
+
+#include <lf/quad/quad.h>
+#include "loc_comp_ellbvp.h"
+
+namespace lf::fe {
+/**
+ * @brief Class for the local computation of the L2 norm of the difference of
+ *        a Lagrangian finite element function and a generic function
+ *
+ * @tparam FUNCTOR type for a generic function
+ *
+ * ### Template parameter type requirements
+ *
+ * - FUNCTOR must behave like `std::function<SCALAR(Eigen::DenseBase)>` where
+ *   SCALAR is a field type.
+
+ *
+ * This class complies with the type requirements for the template argument
+ * LOCAL_NORM_COMP of the function lf::fe::L2NormDifference. Its main method
+ * is the evaluation operator.
+ *
+ * ### Use case
+ *
+ * ~~~
+ *   LocalL2NormDifference loc_comp(
+ *     fe_space.TriaShapeFunctionLayout(), fe_space.QuadShapeFunctionLayout(),
+ *      [](auto x) { return (x[0] * (1.0 - x[1])); }, quad_order);
+ * ~~~
+ */
+template <typename FUNCTOR>
+class LocalL2NormDifference : public LocCompLagrFEPreprocessor {
+ public:
+  /** @brief Constructor
+   *
+   * @param fe_tria reference to layout description for reference shape
+   * functions on triangular cells
+   * @param fe_quad reference to layout description for reference shape
+   * functions on quadrilateral cells
+   * @param u object providing scalar-valued function
+   * @param loc_quad_order desired order of local quadrature, default value = 0.
+   *        If = 0, the quadrature order is determined from the polynomial
+   *        degree of the reference shape functions.
+   *
+   */
+  LocalL2NormDifference(const ScalarReferenceFiniteElement<double> &fe_tria,
+                        const ScalarReferenceFiniteElement<double> &fe_quad,
+                        FUNCTOR u, lf::quad::quadOrder_t loc_quad_order = 0)
+      : LocCompLagrFEPreprocessor(fe_tria, fe_quad, loc_quad_order), u_(u) {}
+  /*
+   * @brief main function for the computation of **squared** local L2 norms
+   *
+   * @tparam DOFVECTOR vector type for passing local coefficient vector
+   *
+   * @param cell reference to the (triangular or quadrilateral) cell for
+   *        which the element matrix should be computed.
+   * @param dofs vector of degrees of freedom for the current entity
+   * @return the local "error norm" squared
+   *
+   * Actual computation of the element matrix based on numerical quadrature and
+   * mapping techniques. The order of the quadrature rule is tied to the
+   * polynomial degree of the underlying Lagrangian finite element spaces: for
+   * polynomial degree p a quadrature rule is chosen that is exact for
+   * polynomials o degree 2p.
+   *
+   * ### Template parameter type requirements
+   *
+   * - DOFVECTOR must be compatible with `Eigen::VectorXd`
+   */
+  template <typename DOFVECTOR>
+  double operator()(const lf::mesh::Entity &cell, const DOFVECTOR &dofs);
+
+ private:
+  /** @brief a handle to the generic function with which to compare the finite
+   * element function */
+  FUNCTOR u_;
+
+ public:
+  /** @brief output control variable */
+  static unsigned int ctrl_;
+  static const unsigned int kout_cell = 8;
+  static const unsigned int kout_comp = 16;
+};
+
+// Initialization of control variable
+template <typename FUNCTOR>
+unsigned int LocalL2NormDifference<FUNCTOR>::ctrl_ = 0;
+
+template <typename FUNCTOR>
+template <typename DOFVECTOR>
+double LocalL2NormDifference<FUNCTOR>::operator()(const lf::mesh::Entity &cell,
+                                                  const DOFVECTOR &dofs) {
+  using dof_t = typename DOFVECTOR::value_type;
+  // Topological type of the cell
+  const lf::base::RefEl ref_el{cell.RefEl()};
+  // Query the shape of the cell
+  const lf::geometry::Geometry *geo_ptr = cell.Geometry();
+  LF_ASSERT_MSG(geo_ptr != nullptr, "Invalid geometry!");
+  LF_ASSERT_MSG((geo_ptr->DimGlobal() == 2) && (geo_ptr->DimLocal() == 2),
+                "Only 2D implementation available!");
+  LF_ASSERT_MSG(dofs.size() >= num_rsf(ref_el), "Dof vector too short");
+  // Obtain cell--type depend invariant data
+  const Eigen::MatrixXd &qr_Points{qr_points(ref_el)};
+  const Eigen::VectorXd &qr_Weights{qr_weights(ref_el)};
+  const size_type qr_NumPts = qr_num_pts(ref_el);
+  const size_type num_LSF = num_rsf(ref_el);
+  const auto rsf_QuadPts{rsf_at_quadpts(ref_el)};
+  // const auto grad_QuadPts{grad_rsf_at_quadpts(ref_el)};
+
+  SWITCHEDSTATEMENT(ctrl_, kout_cell,
+                    std::cout << ref_el << ", (Nlsf = " << num_LSF
+                              << ", Nqr = " << qr_NumPts << ") shape = \n"
+                              << geo_ptr->Global(ref_el.NodeCoords())
+                              << std::endl);
+
+  // World coordinates of quadrature points
+  const Eigen::MatrixXd mapped_qpts(geo_ptr->Global(qr_Points));
+  // Obtain the metric factors for the quadrature points
+  const Eigen::VectorXd determinants(geo_ptr->IntegrationElement(qr_Points));
+  SWITCHEDSTATEMENT(
+      ctrl_, kout_comp, std::cout << "\t dofs = "; for (auto x
+                                                        : dofs) {
+        std::cout << x << " ";
+      } std::cout << "\t dets "
+                  << determinants.transpose() << std::endl);
+
+  double sum = 0.0;  // summation variable
+  // Loop over quadrature points
+  for (int k = 0; k < qr_NumPts; ++k) {
+    // Comparison function value at quadrature point
+    const auto uval = u_(mapped_qpts.col(k));
+    // Value of finite element function at quadrature point
+    // Loop over local shape functions/dofs to compute the value of
+    // the finite element function in the quadrature point
+    dof_t uh_val = 0.0;
+    for (int j = 0; j < num_LSF; ++j) {
+      uh_val += rsf_QuadPts(j, k) * dofs[j];
+    }
+    // form the difference
+    uh_val -= uval;
+    // sum the quared modulus weighted with quadrature weight and metric factor
+    sum += qr_Weights[k] * determinants[k] * std::fabs(uh_val * uh_val);
+    SWITCHEDSTATEMENT(ctrl_, kout_comp,
+                      std::cout << "\t @ " << (mapped_qpts.col(k)).transpose()
+                                << ": uh = " << uh_val << ", u = " << uval
+                                << std::endl);
+  }
+  SWITCHEDSTATEMENT(ctrl_, kout_comp,
+                    std::cout << "\t loc norm^2 = " << sum << std::endl);
+  return sum;
+}
+
+// **********************************************************************
+
+/**
+ * @brief Class for the local computation of the L2 norm of the difference of
+ *        the _gradient_ of a Lagrangian finite element function and a
+ *        generic vector field
+ *
+ * @tparam VEC_FUNC type for a generic vector field
+ *
+ * ### Template parameter type requirements
+ *
+ * - VEC_FUNC must behave like `std::function<VECTOR(Eigen::DenseBase)>` where
+ *   VECTOR is a vector type like a vector of Eigen or std::vector.
+ *
+ * This class complies with the type requirements for the template argument
+ * LOCAL_NORM_COMP of the function lf::fe::L2NormDifference. Its main method
+ * is the evaluation operator returning the **squared** local norm.
+ *
+ * @note Only for planar geometies (world dimension = 2)!
+ *
+ * ### Use case
+ *
+ * ~~~
+ * todo
+ * ~~~
+ */
+template <typename VEC_FUNC>
+class LocL2GradientFEDifference : public LocCompLagrFEPreprocessor {
+ public:
+  /** @brief type of return value of generic function */
+  using vecval_t = typename std::invoke_result<VEC_FUNC, Eigen::Vector2d>::type;
+
+  /** @brief Constructor
+   *
+   * @param fe_tria reference to layout description for reference shape
+   * functions on triangular cells
+   * @param fe_quad reference to layout description for reference shape
+   * functions on quadrilateral cells
+   * @param vecfield object providing a generic vector field
+   * @param loc_quad_order desired order of local quadrature, default value = 0.
+   *        If = 0, the quadrature order is determined from the polynomial
+   *        degree of the reference shape functions.
+   *
+   */
+  LocL2GradientFEDifference(const ScalarReferenceFiniteElement<double> &fe_tria,
+                            const ScalarReferenceFiniteElement<double> &fe_quad,
+                            VEC_FUNC vecfield,
+                            lf::quad::quadOrder_t loc_quad_order = 0)
+      : LocCompLagrFEPreprocessor(fe_tria, fe_quad, loc_quad_order),
+        vecfield_(vecfield) {}
+  /*
+   * @brief main function for the computation of **squared** local L2 norms
+   *
+   * @tparam DOFVECTOR vector type for passing local coefficient vector
+   *
+   * @param cell reference to the (triangular or quadrilateral) cell for
+   *        which the element matrix should be computed.
+   * @param dofs vector of degrees of freedom for the current entity
+   * @return the local "error norm" squared
+   *
+   * Actual computation of the element matrix based on numerical quadrature and
+   * mapping techniques. The order of the quadrature rule is tied to the
+   * polynomial degree of the underlying Lagrangian finite element spaces: for
+   * polynomial degree p a quadrature rule is chosen that is exact for
+   * polynomials of degree 2p ("moderate overkill quadrature")
+   *
+   * ### Template parameter type requirements
+   *
+   * - DOFVECTOR must be compatible with `Eigen::VectorXd`
+   */
+  template <typename DOFVECTOR>
+  double operator()(const lf::mesh::Entity &cell, const DOFVECTOR &dofs);
+
+ private:
+  /** @brief a handle to the generic function with which to compare the finite
+   * element function */
+  VEC_FUNC vecfield_;
+
+ public:
+  /** @brief output control variable */
+  static unsigned int ctrl_;
+  static const unsigned int kout_cell = 8;
+  static const unsigned int kout_comp = 16;
+};
+
+// Initialization of control variable
+template <typename VEC_FUNC>
+unsigned int LocL2GradientFEDifference<VEC_FUNC>::ctrl_ = 0;
+
+template <typename VEC_FUNC>
+template <typename DOFVECTOR>
+double LocL2GradientFEDifference<VEC_FUNC>::operator()(
+    const lf::mesh::Entity &cell, const DOFVECTOR &dofs) {
+  using dof_t = typename DOFVECTOR::value_type;
+  // Topological type of the cell
+  const lf::base::RefEl ref_el{cell.RefEl()};
+  // Query the shape of the cell
+  const lf::geometry::Geometry *geo_ptr = cell.Geometry();
+  LF_ASSERT_MSG(geo_ptr != nullptr, "Invalid geometry!");
+  LF_ASSERT_MSG((geo_ptr->DimGlobal() == 2) && (geo_ptr->DimLocal() == 2),
+                "Only 2D implementation available!");
+  LF_ASSERT_MSG(dofs.size() >= num_rsf(ref_el), "Dof vector too short");
+
+  // Obtain cell--type depend but cell-independent data
+  const Eigen::MatrixXd &qr_Points{qr_points(ref_el)};
+  const Eigen::VectorXd &qr_Weights{qr_weights(ref_el)};
+  const size_type qr_NumPts = qr_num_pts(ref_el);
+  const size_type num_LSF = num_rsf(ref_el);
+  const auto gradrsf_QuadPts{grad_rsf_at_quadpts(ref_el)};
+
+  SWITCHEDSTATEMENT(ctrl_, kout_cell,
+                    std::cout << ref_el << ", (Nlsf = " << num_LSF
+                              << ", Nqr = " << qr_NumPts << ") shape = \n"
+                              << geo_ptr->Global(ref_el.NodeCoords())
+                              << std::endl);
+
+  // World coordinates of quadrature points
+  const Eigen::MatrixXd mapped_qpts(geo_ptr->Global(qr_Points));
+  // Obtain the metric factors for the quadrature points
+  const Eigen::VectorXd determinants(geo_ptr->IntegrationElement(qr_Points));
+  // Fetch the transformation matrices for gradients
+  const Eigen::MatrixXd JinvT(geo_ptr->JacobianInverseGramian(qr_Points));
+
+  SWITCHEDSTATEMENT(
+      ctrl_, kout_comp, std::cout << "\t dofs = "; for (auto x
+                                                        : dofs) {
+        std::cout << x << " ";
+      } std::cout << "\t dets "
+                  << determinants.transpose() << std::endl);
+
+  double sum = 0.0;  // summation variable
+  // Loop over quadrature points
+  for (int k = 0; k < qr_NumPts; ++k) {
+    // Value of the vector field at quadrature point
+    const auto vfval = vecfield_(mapped_qpts.col(k));
+    // Value of the gradient of the finite element function at quadrature point
+    // Transformed gradients
+    const auto trf_grad(JinvT.block(0, 2 * k, 2, 2) * gradrsf_QuadPts[k]);
+    // Loop over local shape functions/dofs to compute the value of
+    // the finite element function in the quadrature point
+    Eigen::Matrix<dof_t, 2, 1> grad_uh_val(2, 1);
+    grad_uh_val.setZero();
+    for (int j = 0; j < num_LSF; ++j) {
+      grad_uh_val += (dofs[j] * trf_grad.col(j));
+    }
+    // form the difference
+    grad_uh_val[0] -= vfval[0];
+    grad_uh_val[1] -= vfval[1];
+    // sum the quared modulus weighted with quadrature weight and metric factor
+    sum += qr_Weights[k] * determinants[k] * grad_uh_val.squaredNorm();
+    // clang-format on
+    SWITCHEDSTATEMENT(ctrl_, kout_comp,
+                      std::cout << "\t @ " << (mapped_qpts.col(k)).transpose()
+                                << ": grad uh = [" << grad_uh_val.transpose()
+                                << "[, vf = [" << vfval[0] << ' ' << vfval[1]
+                                << "]" << std::endl);
+    // clang-format on
+  }
+  SWITCHEDSTATEMENT(ctrl_, kout_comp,
+                    std::cout << "\t loc norm^2 = " << sum << std::endl);
+  return sum;
+}
+
+}  // namespace lf::fe
+
+#endif
