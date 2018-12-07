a76546fd2c8b192b4e9a918a50a98c6cb794f064
ralfh <ralfh@ethz.ch>
2018-12-07 10:58:00 +0100

    Fixed error in matlab driver script

--- examples/mesh/matlab_testmesh_plot_driver.m
+++ examples/mesh/matlab_testmesh_plot_driver.m
@@ -6,29 +6,29 @@ display('(Utility function plot_lf_mesh() must be available)');
 
 opts.numbers = true;
 
-display("Test mesh 0");
+display('Test mesh 0');
 plot_lf_mesh(@test_mesh_0,opts);
-print -dpng "test_mesh_0.png";
+print -dpng 'test_mesh_0.png';
 
-display("Test mesh 1");
+display('Test mesh 1');
 plot_lf_mesh(@test_mesh_1,opts);
-print -dpng "test_mesh_1.png";
+print -dpng 'test_mesh_1.png';
 
-display("Test mesh 2");
+display('Test mesh 2');
 plot_lf_mesh(@test_mesh_2,opts);
-print -dpng "test_mesh_2.png";
+print -dpng 'test_mesh_2.png';
 
-display("Test mesh 3");
+display('Test mesh 3');
 plot_lf_mesh(@test_mesh_3,opts);
-print -dpng "test_mesh_3.png";
+print -dpng 'test_mesh_3.png';
 
-display("Test mesh 4");
+display('Test mesh 4');
 plot_lf_mesh(@test_mesh_4,opts);
-print -dpng "test_mesh_4.png";
+print -dpng 'test_mesh_4.png';
 
-display("Test mesh 5");
+display('Test mesh 5');
 plot_lf_mesh(@test_mesh_5,opts);
-print -dpng "test_mesh_5.png";
+print -dpng 'test_mesh_5.png';
 
 
 
