cb402299c851b583a8eb0e289941310c5b23a714
Anian Ruoss <anian.ruoss@gmail.com>
2019-02-04 19:02:10 +0100

    Extend geometry tests to 3D

--- lib/lf/geometry/test/geometry_tests.cc
+++ lib/lf/geometry/test/geometry_tests.cc
@@ -334,226 +334,254 @@ void checkChildGeometry(
 }
 
 TEST(Geometry, Point) {
-  Eigen::MatrixXd global_nodes(2, 1);
-  global_nodes << 1, 2;
-  lf::geometry::Point geom(global_nodes);
+  Eigen::MatrixXd global_nodes_2D(2, 1);
+  global_nodes_2D << 1, 2;
+  Eigen::MatrixXd global_nodes_3D(3, 1);
+  global_nodes_3D << 3, 4, 0;
 
-  // QuadRule is not implemented and coordinate values are irrelevant
-  Eigen::MatrixXd points = Eigen::MatrixXd::Random(0, 3);
-  runGeometryChecks(geom, points, 1e-9);
+  for (const auto &global_nodes : {global_nodes_2D, global_nodes_3D}) {
+    lf::geometry::Point geom(global_nodes);
 
-  // check that local nodes are mapped to global nodes
-  EXPECT_TRUE(
-      geom.Global(Eigen::Matrix<double, 0, 1>()).isApprox(global_nodes));
+    // QuadRule is not implemented and coordinate values are irrelevant
+    Eigen::MatrixXd points = Eigen::MatrixXd::Random(0, 3);
+    runGeometryChecks(geom, points, 1e-9);
+
+    // check that local nodes are mapped to global nodes
+    EXPECT_TRUE(
+        geom.Global(Eigen::Matrix<double, 0, 1>()).isApprox(global_nodes));
+  }
 }
 
 TEST(Geometry, SegmentO1) {
-  Eigen::MatrixXd global_nodes(2, 2);
-  global_nodes << 1, 1, 0, 4;
-  lf::geometry::SegmentO1 geom(global_nodes);
-  auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kSegment(), 5);
-  runGeometryChecks(geom, qr.Points(), 1e-9);
+  Eigen::MatrixXd global_nodes_2D(2, 2);
+  global_nodes_2D << 1, 1, 0, 4;
+  Eigen::MatrixXd global_nodes_3D(3, 2);
+  global_nodes_3D << -1, 10, 1, 3, 2, 3;
 
-  std::vector<lf::refinement::RefPat> segSymmetricRefPats = {
-      lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
-      lf::refinement::RefPat::rp_split};
+  for (const auto &global_nodes : {global_nodes_2D, global_nodes_3D}) {
+    lf::geometry::SegmentO1 geom(global_nodes);
+    auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kSegment(), 5);
+    runGeometryChecks(geom, qr.Points(), 1e-9);
 
-  for (const auto &refPat : segSymmetricRefPats) {
-    checkChildGeometryVolume(geom, refPat);
-    checkChildGeometry(
-        geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
-        [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
+    std::vector<lf::refinement::RefPat> segSymmetricRefPats = {
+        lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
+        lf::refinement::RefPat::rp_split};
+
+    for (const auto &refPat : segSymmetricRefPats) {
+      checkChildGeometryVolume(geom, refPat);
+      checkChildGeometry(
+          geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
+          [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
+    }
+
+    // check that local nodes are mapped to global nodes
+    EXPECT_TRUE(geom.Global(lf::base::RefEl::kSegment().NodeCoords())
+                    .isApprox(global_nodes));
   }
-
-  // check that local nodes are mapped to global nodes
-  EXPECT_TRUE(geom.Global(lf::base::RefEl::kSegment().NodeCoords())
-                  .isApprox(global_nodes));
 }
 
 TEST(Geometry, SegmentO2) {
-  Eigen::MatrixXd global_nodes(2, 3);
-  global_nodes << 1, 2, 3, 1, 4, 2;
+  Eigen::MatrixXd global_nodes_2D(2, 3);
+  global_nodes_2D << 1, 2, 3, 1, 4, 2;
+  Eigen::MatrixXd global_nodes_3D(3, 3);
+  global_nodes_3D << -1, 3, 1, 1, 2, 3, -1, 1, 0;
 
-  lf::geometry::SegmentO2 geom(global_nodes);
-  auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kSegment(), 5);
-  runGeometryChecks(geom, qr.Points(), 1e-9);
+  for (const auto &global_nodes : {global_nodes_2D, global_nodes_3D}) {
+    lf::geometry::SegmentO2 geom(global_nodes);
+    auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kSegment(), 5);
+    runGeometryChecks(geom, qr.Points(), 1e-9);
 
-  std::vector<lf::refinement::RefPat> segSymmetricRefPats = {
-      lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
-      lf::refinement::RefPat::rp_split};
+    std::vector<lf::refinement::RefPat> segSymmetricRefPats = {
+        lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
+        lf::refinement::RefPat::rp_split};
 
-  for (const auto &refPat : segSymmetricRefPats) {
-    checkChildGeometryVolume(geom, refPat);
-    checkChildGeometry(
-        geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
-        [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
+    for (const auto &refPat : segSymmetricRefPats) {
+      checkChildGeometryVolume(geom, refPat);
+      checkChildGeometry(
+          geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
+          [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
+    }
+
+    Eigen::MatrixXd local_nodes(1, 3);
+    local_nodes << 0, 1, 0.5;
+    EXPECT_TRUE(geom.Global(local_nodes).isApprox(global_nodes));
   }
-
-  Eigen::MatrixXd local_nodes(1, 3);
-  local_nodes << 0, 1, 0.5;
-  EXPECT_TRUE(geom.Global(local_nodes).isApprox(global_nodes));
 }
 
 TEST(Geometry, TriaO1) {
-  Eigen::MatrixXd global_nodes(2, 3);
-  global_nodes << 1, 4, 3, 1, 2, 5;
+  Eigen::MatrixXd global_nodes_2D(2, 3);
+  global_nodes_2D << 1, 4, 3, 1, 2, 5;
+  Eigen::MatrixXd global_nodes_3D(3, 3);
+  global_nodes_3D << -1, 2, 0, 0, -2, -7, 3, 3, 5;
 
-  lf::geometry::TriaO1 geom(global_nodes);
-  auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 5);
-  runGeometryChecks(geom, qr.Points(), 1e-9);
+  for (const auto &global_nodes : {global_nodes_2D, global_nodes_3D}) {
+    lf::geometry::TriaO1 geom(global_nodes);
+    auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 5);
+    runGeometryChecks(geom, qr.Points(), 1e-9);
 
-  std::vector<lf::refinement::RefPat> triaSymmetricRefPats = {
-      lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
-      lf::refinement::RefPat::rp_regular,
-      lf::refinement::RefPat::rp_barycentric};
+    std::vector<lf::refinement::RefPat> triaSymmetricRefPats = {
+        lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
+        lf::refinement::RefPat::rp_regular,
+        lf::refinement::RefPat::rp_barycentric};
 
-  for (const auto &refPat : triaSymmetricRefPats) {
-    checkChildGeometryVolume(geom, refPat);
-    checkChildGeometry(
-        geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
-        [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
-  }
-
-  std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
-      lf::refinement::RefPat::rp_bisect, lf::refinement::RefPat::rp_trisect,
-      lf::refinement::RefPat::rp_trisect_left,
-      lf::refinement::RefPat::rp_quadsect};
-
-  for (const auto &refPat : triaAsymmetricRefPats) {
-    for (size_t anchor = 0; anchor < 3; ++anchor) {
-      checkChildGeometryVolume(geom, refPat, anchor);
+    for (const auto &refPat : triaSymmetricRefPats) {
+      checkChildGeometryVolume(geom, refPat);
       checkChildGeometry(
-          geom,
-          lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat,
-                                                    anchor),
+          geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
           [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
     }
-  }
 
-  // check that local nodes are mapped to global nodes
-  EXPECT_TRUE(geom.Global(lf::base::RefEl::kTria().NodeCoords())
-                  .isApprox(global_nodes));
+    std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
+        lf::refinement::RefPat::rp_bisect, lf::refinement::RefPat::rp_trisect,
+        lf::refinement::RefPat::rp_trisect_left,
+        lf::refinement::RefPat::rp_quadsect};
+
+    for (const auto &refPat : triaAsymmetricRefPats) {
+      for (size_t anchor = 0; anchor < 3; ++anchor) {
+        checkChildGeometryVolume(geom, refPat, anchor);
+        checkChildGeometry(
+            geom,
+            lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat,
+                                                      anchor),
+            [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
+      }
+    }
+
+    // check that local nodes are mapped to global nodes
+    EXPECT_TRUE(geom.Global(lf::base::RefEl::kTria().NodeCoords())
+                    .isApprox(global_nodes));
+  }
 }
 
 TEST(Geometry, TriaO2) {
-  Eigen::MatrixXd global_nodes(2, 6);
-  global_nodes << 1, 4, 3, 2, 4, 0, 2, 1, 5, 3, 4, 4;
+  Eigen::MatrixXd global_nodes_2D(2, 6);
+  global_nodes_2D << 1, 4, 3, 2, 4, 0, 2, 1, 5, 3, 4, 4;
+  Eigen::MatrixXd global_nodes_3D(3, 6);
+  global_nodes_3D << 2, 0, -3, 1, -1, 0, -2, 0, -3, -1.5, -1, -2.8, 0, 5, 1, 2,
+      3, .2;
 
-  lf::geometry::TriaO2 geom(global_nodes);
-  auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 5);
-  runGeometryChecks(geom, qr.Points(), 1e-9);
+  for (const auto &global_nodes : {global_nodes_2D, global_nodes_3D}) {
+    lf::geometry::TriaO2 geom(global_nodes);
+    auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 5);
+    runGeometryChecks(geom, qr.Points(), 1e-9);
 
-  std::vector<lf::refinement::RefPat> triaSymmetricRefPats = {
-      lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
-      lf::refinement::RefPat::rp_regular,
-      lf::refinement::RefPat::rp_barycentric};
+    std::vector<lf::refinement::RefPat> triaSymmetricRefPats = {
+        lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
+        lf::refinement::RefPat::rp_regular,
+        lf::refinement::RefPat::rp_barycentric};
 
-  for (const auto &refPat : triaSymmetricRefPats) {
-    checkChildGeometryVolume(geom, refPat);
-    checkChildGeometry(
-        geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
-        [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
-  }
-
-  std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
-      lf::refinement::RefPat::rp_bisect, lf::refinement::RefPat::rp_trisect,
-      lf::refinement::RefPat::rp_trisect_left,
-      lf::refinement::RefPat::rp_quadsect};
-
-  for (const auto &refPat : triaAsymmetricRefPats) {
-    for (size_t anchor = 0; anchor < 3; ++anchor) {
-      checkChildGeometryVolume(geom, refPat, anchor);
+    for (const auto &refPat : triaSymmetricRefPats) {
+      checkChildGeometryVolume(geom, refPat);
       checkChildGeometry(
-          geom,
-          lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat,
-                                                    anchor),
+          geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
           [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
     }
-  }
 
-  // check that local nodes are mapped to global nodes
-  Eigen::MatrixXd local_nodes(2, 6);
-  local_nodes << 0, 1, 0, 0.5, 0.5, 0, 0, 0, 1, 0, 0.5, 0.5;
-  EXPECT_TRUE(geom.Global(local_nodes).isApprox(global_nodes));
+    std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
+        lf::refinement::RefPat::rp_bisect, lf::refinement::RefPat::rp_trisect,
+        lf::refinement::RefPat::rp_trisect_left,
+        lf::refinement::RefPat::rp_quadsect};
+
+    for (const auto &refPat : triaAsymmetricRefPats) {
+      for (size_t anchor = 0; anchor < 3; ++anchor) {
+        checkChildGeometryVolume(geom, refPat, anchor);
+        checkChildGeometry(
+            geom,
+            lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat,
+                                                      anchor),
+            [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
+      }
+    }
+
+    // check that local nodes are mapped to global nodes
+    Eigen::MatrixXd local_nodes(2, 6);
+    local_nodes << 0, 1, 0, 0.5, 0.5, 0, 0, 0, 1, 0, 0.5, 0.5;
+    EXPECT_TRUE(geom.Global(local_nodes).isApprox(global_nodes));
+  }
 }
 
 TEST(Geometry, QuadO1) {
-  Eigen::MatrixXd global_nodes(2, 4);
-  global_nodes << -1, 3, 2, 1, -2, 0, 2, 1;
+  Eigen::MatrixXd global_nodes_2D(2, 4);
+  global_nodes_2D << -1, 3, 2, 1, -2, 0, 2, 1;
 
-  lf::geometry::QuadO1 geom(global_nodes);
-  auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kQuad(), 5);
-  runGeometryChecks(geom, qr.Points(), 1e-9);
+  for (const auto &global_nodes : {global_nodes_2D}) {
+    lf::geometry::QuadO1 geom(global_nodes);
+    auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kQuad(), 5);
+    runGeometryChecks(geom, qr.Points(), 1e-9);
 
-  std::vector<lf::refinement::RefPat> quadSymmetricRefPats = {
-      lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
-      lf::refinement::RefPat::rp_regular,
-      lf::refinement::RefPat::rp_barycentric};
+    std::vector<lf::refinement::RefPat> quadSymmetricRefPats = {
+        lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
+        lf::refinement::RefPat::rp_regular,
+        lf::refinement::RefPat::rp_barycentric};
 
-  for (const auto &refPat : quadSymmetricRefPats) {
-    checkChildGeometryVolume(geom, refPat);
-    checkChildGeometry(
-        geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
-        [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
-  }
-
-  std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
-      lf::refinement::RefPat::rp_split, lf::refinement::RefPat::rp_bisect,
-      lf::refinement::RefPat::rp_trisect, lf::refinement::RefPat::rp_quadsect,
-      lf::refinement::RefPat::rp_threeedge};
-
-  for (const auto &refPat : triaAsymmetricRefPats) {
-    for (size_t anchor = 0; anchor < 4; ++anchor) {
-      checkChildGeometryVolume(geom, refPat, anchor);
-      checkChildGeometry(geom,
-                         lf::refinement::Hybrid2DRefinementPattern(
-                             geom.RefEl(), refPat, anchor),
-                         lf::quad::make_QuadRuleNodal);
+    for (const auto &refPat : quadSymmetricRefPats) {
+      checkChildGeometryVolume(geom, refPat);
+      checkChildGeometry(
+          geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
+          [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
     }
-  }
 
-  // check that local nodes are mapped to global nodes
-  EXPECT_TRUE(geom.Global(lf::base::RefEl::kQuad().NodeCoords())
-                  .isApprox(global_nodes));
+    std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
+        lf::refinement::RefPat::rp_split, lf::refinement::RefPat::rp_bisect,
+        lf::refinement::RefPat::rp_trisect, lf::refinement::RefPat::rp_quadsect,
+        lf::refinement::RefPat::rp_threeedge};
+
+    for (const auto &refPat : triaAsymmetricRefPats) {
+      for (size_t anchor = 0; anchor < 4; ++anchor) {
+        checkChildGeometryVolume(geom, refPat, anchor);
+        checkChildGeometry(geom,
+                           lf::refinement::Hybrid2DRefinementPattern(
+                               geom.RefEl(), refPat, anchor),
+                           lf::quad::make_QuadRuleNodal);
+      }
+    }
+
+    // check that local nodes are mapped to global nodes
+    EXPECT_TRUE(geom.Global(lf::base::RefEl::kQuad().NodeCoords())
+                    .isApprox(global_nodes));
+  }
 }
 
 TEST(Geometry, QuadO2) {
-  Eigen::MatrixXd global_nodes(2, 8);
-  global_nodes << 3, 7, 4, 1, 5, 5.4, 2.5, 1.8, 1, 3, 7, 8, 2.5, 5.9, 6.9, 4.1;
+  Eigen::MatrixXd global_nodes_2D(2, 8);
+  global_nodes_2D << 3, 7, 4, 1, 5, 5.4, 2.5, 1.8, 1, 3, 7, 8, 2.5, 5.9, 6.9,
+      4.1;
 
-  lf::geometry::QuadO2 geom(global_nodes);
-  auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kQuad(), 5);
-  runGeometryChecks(geom, qr.Points(), 1e-9);
+  for (const auto &global_nodes : {global_nodes_2D}) {
+    lf::geometry::QuadO2 geom(global_nodes);
+    auto qr = lf::quad::make_QuadRule(lf::base::RefEl::kQuad(), 5);
+    runGeometryChecks(geom, qr.Points(), 1e-9);
 
-  std::vector<lf::refinement::RefPat> quadSymmetricRefPats = {
-      lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
-      lf::refinement::RefPat::rp_regular,
-      lf::refinement::RefPat::rp_barycentric};
+    std::vector<lf::refinement::RefPat> quadSymmetricRefPats = {
+        lf::refinement::RefPat::rp_nil, lf::refinement::RefPat::rp_copy,
+        lf::refinement::RefPat::rp_regular,
+        lf::refinement::RefPat::rp_barycentric};
 
-  for (const auto &refPat : quadSymmetricRefPats) {
-    checkChildGeometryVolume(geom, refPat);
-    checkChildGeometry(
-        geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
-        [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
-  }
-
-  std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
-      lf::refinement::RefPat::rp_split, lf::refinement::RefPat::rp_bisect,
-      lf::refinement::RefPat::rp_trisect, lf::refinement::RefPat::rp_quadsect,
-      lf::refinement::RefPat::rp_threeedge};
-
-  for (const auto &refPat : triaAsymmetricRefPats) {
-    for (size_t anchor = 0; anchor < 4; ++anchor) {
-      checkChildGeometryVolume(geom, refPat, anchor);
-      checkChildGeometry(geom,
-                         lf::refinement::Hybrid2DRefinementPattern(
-                             geom.RefEl(), refPat, anchor),
-                         lf::quad::make_QuadRuleNodal);
+    for (const auto &refPat : quadSymmetricRefPats) {
+      checkChildGeometryVolume(geom, refPat);
+      checkChildGeometry(
+          geom, lf::refinement::Hybrid2DRefinementPattern(geom.RefEl(), refPat),
+          [](auto ref_el) { return lf::quad::make_QuadRule(ref_el, 5); });
     }
-  }
 
-  // check that local nodes are mapped to global nodes
-  Eigen::MatrixXd local_nodes(2, 8);
-  local_nodes << 0, 1, 1, 0, .5, 1, .5, 0, 0, 0, 1, 1, 0, .5, 1, 0.5;
-  EXPECT_TRUE(geom.Global(local_nodes).isApprox(global_nodes));
+    std::vector<lf::refinement::RefPat> triaAsymmetricRefPats = {
+        lf::refinement::RefPat::rp_split, lf::refinement::RefPat::rp_bisect,
+        lf::refinement::RefPat::rp_trisect, lf::refinement::RefPat::rp_quadsect,
+        lf::refinement::RefPat::rp_threeedge};
+
+    for (const auto &refPat : triaAsymmetricRefPats) {
+      for (size_t anchor = 0; anchor < 4; ++anchor) {
+        checkChildGeometryVolume(geom, refPat, anchor);
+        checkChildGeometry(geom,
+                           lf::refinement::Hybrid2DRefinementPattern(
+                               geom.RefEl(), refPat, anchor),
+                           lf::quad::make_QuadRuleNodal);
+      }
+    }
+
+    // check that local nodes are mapped to global nodes
+    Eigen::MatrixXd local_nodes(2, 8);
+    local_nodes << 0, 1, 1, 0, .5, 1, .5, 0, 0, 0, 1, 1, 0, .5, 1, 0.5;
+    EXPECT_TRUE(geom.Global(local_nodes).isApprox(global_nodes));
+  }
 }
