4fb197a381a98457a5b5970a8585d092dc3aa97d
ralfh <ralfh@ethz.ch>
2019-02-08 07:07:06 +0100

    Added outut of tensor-product meshes to test_mesh_output_demo

--- examples/io/test_mesh_output/test_mesh_output_demo.cc
+++ examples/io/test_mesh_output/test_mesh_output_demo.cc
@@ -11,8 +11,54 @@
 #include "lf/mesh/test_utils/test_meshes.h"
 #include "lf/mesh/utils/utils.h"
 
-int main() {
+/**
+ * @brief "graphical" output of test meshes in various formats
+ * @param mesh_p pointer to LehrFEM++ mesh object to be rendered
+ * @param filename name of the file to be written. An appropriate
+ * suffix will be added.
+ *
+ * This function writes the mesh geometry and topology information
+ * to file in various formats for graphical rendering:
+ * - LaTeX TikZ format, stored in `.tex` file
+ * - Matlab format, to be rendered by Matlab function  `plot_lf_mesh.m`
+ * - Python format meant for Python script 'plot_mesh.py'
+ * - vtk format for rendering with Paraview
+ *
+ * @note We have to pass pointer to the mesh, because vtk output requires the
+ * the construction of a writer object that holds a shared pointer to the mesh.
+ */
+void writeMeshRenderingData(const std::shared_ptr<const lf::mesh::Mesh> mesh_p,
+                            const char *filename);
+
+void writeMeshRenderingData(const std::shared_ptr<const lf::mesh::Mesh> mesh_p,
+                            const char *filename) {
+  // TikZ output
   using lf::io::TikzOutputCtrl;
+  std::stringstream filename_tikz;
+  filename_tikz << filename << ".tex";
+  lf::io::writeTikZ(
+      *mesh_p, filename_tikz.str(),
+      TikzOutputCtrl::RenderCells | TikzOutputCtrl::VerticeNumbering |
+          TikzOutputCtrl::EdgeNumbering | TikzOutputCtrl::CellNumbering |
+          TikzOutputCtrl::NodeNumbering | TikzOutputCtrl::ArrowTips);
+
+  // Matlab output
+  std::stringstream filename_matlab;
+  filename_matlab << filename << ".m";
+  lf::io::writeMatlab(*mesh_p, filename_matlab.str());
+
+  // Python output
+  std::stringstream filename_py;
+  filename_py << filename << ".py";
+  lf::io::writeMatplotlib(*mesh_p, filename_py.str());
+
+  // VTK output
+  std::stringstream filename_vtk;
+  filename_vtk << filename << ".vtk";
+  lf::io::VtkWriter vtk_writer(mesh_p, filename_vtk.str());
+}
+
+int main() {
   std::cout << "LehrFEM++ demo: output of test meshes" << std::endl;
   std::cout << "(test meshes from "
                "lf::mesh::test_utils::GenerateHybrid2DTestMesh()"
@@ -22,35 +68,33 @@ int main() {
        selector++) {
     // Generates a small test meshes, more precisely described in the
     // documentation of the function GenerateHybrid2DTestMesh()
-    auto mesh_p = lf::mesh::test_utils::GenerateHybrid2DTestMesh(selector);
+    std::shared_ptr<const lf::mesh::Mesh> mesh_p =
+        lf::mesh::test_utils::GenerateHybrid2DTestMesh(selector);
     // Output of mesh information
     std::cout << "#### Test mesh " << selector << " ####" << std::endl;
     std::cout << *mesh_p << std::endl;
 
-    // TikZ output
-    std::stringstream filename_tikz;
-    filename_tikz << "test_mesh_" << selector << ".tex";
-    lf::io::writeTikZ(
-        *mesh_p, filename_tikz.str(),
-        TikzOutputCtrl::RenderCells | TikzOutputCtrl::VerticeNumbering |
-            TikzOutputCtrl::EdgeNumbering | TikzOutputCtrl::CellNumbering |
-            TikzOutputCtrl::NodeNumbering | TikzOutputCtrl::ArrowTips);
-
-    // Matlab output
-    std::stringstream filename_matlab;
-    filename_matlab << "test_mesh_" << selector << ".m";
-    lf::io::writeMatlab(*mesh_p, filename_matlab.str());
-
-    // Python output
-    std::stringstream filename_py;
-    filename_py << "test_mesh_" << selector << ".py";
-    lf::io::writeMatplotlib(*mesh_p, filename_py.str());
-
-    // VTK output
-    std::stringstream filename_vtk;
-    filename_vtk << "test_mesh_" << selector << ".vtk";
-    lf::io::VtkWriter vtk_writer(mesh_p, filename_vtk.str());
+    std::stringstream filename;
+    filename << "test_mesh_" << selector;
+    writeMeshRenderingData(mesh_p, filename.str().c_str());
   }
 
+  {
+    // Initialize builder object
+    lf::mesh::hybrid2d::TPTriagMeshBuilder builder(
+        std::make_shared<lf::mesh::hybrid2d::MeshFactory>(2));
+    // Set mesh parameters following the Builder pattern
+    // Domain is the unit square, two cells in every direction
+    builder.setBottomLeftCorner(Eigen::Vector2d{0, 0})
+        .setTopRightCorner(Eigen::Vector2d{1, 1})
+        .setNoXCells(2)
+        .setNoYCells(2);
+    std::shared_ptr<const lf::mesh::Mesh> mesh_p = builder.Build();
+    // Output of mesh information
+    std::cout << "#### Triangular tensor product 2x2 mesh #####" << std::endl;
+    std::cout << *mesh_p << std::endl;
+
+    writeMeshRenderingData(mesh_p, "tp_tria_mesh");
+  }
   return 0L;
 }
