d28b010da0a8178166b2bdd372d93f300dc5c301
Cryoris <jules.gacon@googlemail.com>
2019-02-12 08:02:14 +0100

    Replace staticvars via comm

--- examples/mesh/structured_mesh_demo.cc
+++ examples/mesh/structured_mesh_demo.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "lf/base/base.h"
+#include "lf/base/comm.h"
 #include "lf/io/io.h"
 #include "lf/mesh/hybrid2d/hybrid2d.h"
 #include "lf/mesh/mesh.h"
@@ -11,22 +12,32 @@
 #include "lf/mesh/test_utils/test_meshes.h"
 #include "lf/mesh/utils/utils.h"
 
-ADDOPTION(Nx, Nx_cells, "No of cells in X direction");
-ADDOPTION(Ny, Ny_cells, "No of cells in Y direction");
+ADDOPTION(Nx, Nx_cells, "No of cells in X direction, must be > 0");
+ADDOPTION(Ny, Ny_cells, "No of cells in Y direction, must be > 0");
 
-int main(int argc, const char *argv[]) {
+
+int main(int argc, char** argv) {
   using size_type = lf::base::size_type;
 
-  std::cout << "LehrFEM++ demo: construction of tensor producy triangular mesh"
-            << std::endl;
+  std::cout << "LehrFEM++ demo: construction of tensor producy triangular mesh\n"
+            << "Use the option `-h` to display the control variables.\n";
 
   // Set control variables from command line or file "setup vars"
-  lf::base::ReadCtrVarsCmdArgs(argc, argv);
-  if (!lf::base::ReadCtrlVarsFile("setup.vars")) {
-    std::cout << "No file specifyng control variables" << std::endl;
+  lf::base::ci::Init(argc, argv);
+  lf::base::ci::Add("help,h", "Display help");
+  // check for file with options 
+  if (!lf::base::ci::ParseFile("setup.vars")) {
+    std::cout << "No file `setup.vars` specifying control variables\n";
   }
-  std::cout << "##### Control variables:" << std::endl;
-  lf::base::ListCtrlVars(std::cout);
+  lf::base::ci::ParseCommandLine();
+  if (lf::base::ci::Help())
+    return 0;
+
+  // number of cells cannot be 0, that would result in segfault!
+  if (Nx == 0 || Ny == 0) {
+    std::cout << "Nx and Ny must not be zero, set using options --Nx_cells and --Ny_cells\n";
+    return 0;
+  }
 
   // Construct a triangular tensor product mesh with 2*Nx*Ny cells
   lf::mesh::hybrid2d::TPTriagMeshBuilder builder(
