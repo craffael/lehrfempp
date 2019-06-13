a52946634e1377ff5abb7e95d56ff5ddfdf7e5cb
craffael <raffael@casagrande.ch>
2019-06-13 09:19:11 +0200

    try 1.8.15

--- travis/install_doxygen.sh
+++ travis/install_doxygen.sh
@@ -8,15 +8,15 @@ dir=$(pwd)
 
 mkdir -p ${DEPS_DIR} && cd ${DEPS_DIR}
 
-if [ ! -d "doxygen-1.8.14" ]; then
-  wget -O - http://doxygen.nl/files/doxygen-1.8.14.src.tar.gz | tar xz
-  cd doxygen-1.8.14
+if [ ! -d "doxygen-1.8.15" ]; then
+  wget -O - http://doxygen.nl/files/doxygen-1.8.15.src.tar.gz | tar xz
+  cd doxygen-1.8.15
   mkdir build
   cd build
   cmake -Duse_libclang=ON  ..
   make -j2
 fi
-export PATH=${DEPS_DIR}/doxygen-1.8.14/build/bin:$PATH
+export PATH=${DEPS_DIR}/doxygen-1.8.15/build/bin:$PATH
 doxygen --version
 
 #Change back to where we left off.
