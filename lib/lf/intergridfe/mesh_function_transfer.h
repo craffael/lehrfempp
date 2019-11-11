c3c4511f62b08b0a9e0181bef9fbc8101fc5d1d9
Tobias Rohner <torohner@student.ethz.ch>
2019-11-11 22:01:10 +0100

    Created file structure

---
+++ lib/lf/intergridfe/mesh_function_transfer.h
@@ -0,0 +1,12 @@
+#ifndef FE_INTERGRIDFE_MESH_FUNCTION_TRANSFER_H
+#define FE_INTERGRIDFE_MESH_FUNCTION_TRANSFER_H
+
+
+
+namespace lf::intergridfe {
+
+}   // end namespace intergridfe
+
+
+
+#endif // FE_INTERGRIDFE_MESH_FUNCTION_TRANSFER_H
