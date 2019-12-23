8f365b106840b96efa28aa10c266da3fd530fa0b
Tobias Rohner <torohner@student.ethz.ch>
2019-12-23 14:28:05 +0100

    Added test

--- lib/lf/intergridfe/test/transfer_tests.cc
+++ lib/lf/intergridfe/test/transfer_tests.cc
@@ -106,3 +106,62 @@ TEST(lf_intergridfe, NormQuadratic) {
     }
   }
 }
+
+template<typename FES_COARSE, typename FES_FINE>
+void check_lagr_interp_nodes(std::shared_ptr<lf::mesh::Mesh> mesh_coarse, std::shared_ptr<const FES_COARSE> fes_coarse, const Eigen::VectorXd& dofs_coarse, lf::refinement::RefPat ref_pat) {
+    // Refine the mesh using the given refinement pattern
+    auto factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
+    auto mh = lf::refinement::MeshHierarchy(mesh_coarse, std::move(factory));
+    mh.RefineRegular(ref_pat);
+    const auto mesh_fine = mh.getMesh(1);
+    // Initialize the fine fe space
+    const auto fes_fine = std::make_shared<const FES_FINE>(mesh_fine);
+    // Prolongate the given dof vector
+    const auto dofs_fine = lf::intergridfe::prolongate(mh, fes_coarse, fes_fine, dofs_coarse, 0);
+    // Compare the resulting mesh functions at the interpolation nodes
+    lf::uscalfe::MeshFunctionFE mf_coarse(fes_coarse, dofs_coarse);
+    lf::uscalfe::MeshFunctionFE mf_fine(fes_fine, dofs_fine);
+    for (const auto cell : mesh_fine->Entities(0)) {
+	const auto idx = mesh_fine->Index(*cell);
+	const auto layout = fes_fine->ShapeFunctionLayout(cell->RefEl());
+	const auto rel_geom = mh.GeometryInParent(1, *cell);
+	const Eigen::MatrixXd eval_nodes = layout->EvaluationNodes();
+	const Eigen::MatrixXd parent_eval_nodes = rel_geom->Global(eval_nodes);
+	const auto parent = mh.ParentInfos(1, 0)[idx].parent_ptr;
+	const auto val_fine = mf_fine(*cell, eval_nodes);
+	const auto val_coarse = mf_coarse(*parent, parent_eval_nodes);
+	for (int i = 0 ; i < val_fine.size() ; ++i) {
+	    ASSERT_TRUE(std::fabs(val_fine[i] - val_coarse[i]) < 1e-10);
+	}
+    }
+}
+
+TEST(lf_intergridfe, LagrInterpNodes) {
+    for (int selector = 0 ; selector < 9 ; ++selector) {
+	// Initialize a coarse test mesh
+	const auto mesh_coarse = lf::mesh::test_utils::GenerateHybrid2DTestMesh(selector);
+	// Initialize the coarse fe spaces
+	const auto fes_coarse_o1 = std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_coarse);
+	const auto fes_coarse_o2 = std::make_shared<lf::uscalfe::FeSpaceLagrangeO2<double>>(mesh_coarse);
+	// Test for all possible refinement patterns
+	for (const auto ref_pat : {lf::refinement::rp_regular,
+				   lf::refinement::rp_barycentric}) {
+	    const auto ndofs_coarse_o1 = fes_coarse_o1->LocGlobMap().NumDofs();
+	    const auto ndofs_coarse_o2 = fes_coarse_o2->LocGlobMap().NumDofs();
+	    // Check interpolation of first order lagrangian fe
+	    for (unsigned long dof = 0 ; dof < ndofs_coarse_o1 ; ++dof) {
+		Eigen::VectorXd dofs = Eigen::VectorXd::Zero(ndofs_coarse_o1);
+		dofs[dof] = 1;
+		check_lagr_interp_nodes<lf::uscalfe::FeSpaceLagrangeO1<double>, lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_coarse, fes_coarse_o1, dofs, ref_pat);
+		check_lagr_interp_nodes<lf::uscalfe::FeSpaceLagrangeO1<double>, lf::uscalfe::FeSpaceLagrangeO2<double>>(mesh_coarse, fes_coarse_o1, dofs, ref_pat);
+	    }
+	    // Check interpolation of second order lagrangian fe
+	    for (unsigned long dof = 0 ; dof < ndofs_coarse_o2 ; ++dof) {
+		Eigen::VectorXd dofs = Eigen::VectorXd::Zero(ndofs_coarse_o2);
+		dofs[dof] = 1;
+		check_lagr_interp_nodes<lf::uscalfe::FeSpaceLagrangeO2<double>, lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_coarse, fes_coarse_o2, dofs, ref_pat);
+		check_lagr_interp_nodes<lf::uscalfe::FeSpaceLagrangeO2<double>, lf::uscalfe::FeSpaceLagrangeO2<double>>(mesh_coarse, fes_coarse_o2, dofs, ref_pat);
+	    }
+	}
+    }
+}
