29f44f17abafe4c9750f80b6375860e79f1e218d
Tobias Rohner <torohner@student.ethz.ch>
2020-01-17 20:42:31 +0100

    Specify the target mesh at runtime

--- lib/lf/refinement/mesh_function_transfer.h
+++ lib/lf/refinement/mesh_function_transfer.h
@@ -10,6 +10,10 @@
 
 namespace lf::refinement {
 
+/**
+ * @brief A MeshFunction representing interpolation on a lf::refinement::MeshHierarchy
+ * @tparam The type of mesh function to interpolate on a finer mesh
+ */
 template <typename MF>
 class MeshFunctionTransfer {
   using mf_t = std::remove_cv_t<std::remove_reference_t<MF>>;
@@ -26,16 +30,34 @@ class MeshFunctionTransfer {
       decltype(has_getMesh_impl<mf_t>(int{})){};
 
  public:
+  /**
+   * @brief Copy Constructor
+   */
   MeshFunctionTransfer(const MeshFunctionTransfer &) = default;
+
+  /**
+   * @brief Move Constructor
+   */
   MeshFunctionTransfer(MeshFunctionTransfer &&) = default;
+
+  /**
+   * @brief Constructor
+   * @param mh The lf::refinement::MeshHierarchy along which the function should be interpolated
+   * @param mf The mesh function to be interpolated on a finer mesh
+   * @param level_coarse The level of the Mesh which `mf` is defined on
+   * @param level_fine The level of the Mesh this wrapper MeshFunction is defined on
+   */
   MeshFunctionTransfer(const lf::refinement::MeshHierarchy &mh, const MF &mf,
-                       lf::base::size_type level_coarse)
-      : mh_(mh), mf_(mf), level_coarse_(level_coarse) {
+                       lf::base::size_type level_coarse, lf::base::size_type level_fine)
+      : mh_(mh), mf_(mf), level_coarse_(level_coarse), level_fine_(level_fine) {
     // Assert that the `level_coarse` parameter does not point to the finest
     // mesh
     LF_ASSERT_MSG(
         level_coarse < mh.NumLevels() - 1,
         "level_coarse must not point to the finest mesh in the hierarchy");
+    // Assert that the `level_fine` parameter points to a finer mesh than `level_coarse`
+    LF_ASSERT_MSG(level_fine > level_coarse, "level_fine must be bigger than level_fine");
+    LF_ASSERT_MSG(level_fine < mh.NumLevels(), "level_fine must point to a valid mesh in the mesh hierarchy");
     // If the mesh function is defined over an FE space, assert the correctness
     // of the `level_coarse` parameter
     if constexpr (provides_getMesh) {
@@ -44,14 +66,29 @@ class MeshFunctionTransfer {
     }
   }
 
+  /**
+   * @brief Evaluate the MeshFunction
+   * @param e The Entity on which to evaluate this MeshFunction
+   * @param local The local coordinates inside `e` where the MeshFunction should be evaluated
+   * @returns A std::vector of point evaluations of the MeshFunction
+   */
   decltype(auto) operator()(const lf::mesh::Entity &e,
                             const Eigen::MatrixXd &local) const {
-    const auto parent = mh_.ParentEntity(level_coarse_ + 1, e);
-    const auto rel_geom = mh_.GeometryInParent(level_coarse_ + 1, e);
-    const auto local_parent = rel_geom->Global(local);
+    auto rel_geom = mh_.GeometryInParent(level_fine_, e);
+    auto parent = mh_.ParentEntity(level_fine_, e);
+    auto local_parent = rel_geom->Global(local);
+    for (lf::base::size_type lvl = level_fine_-1 ; lvl > level_coarse_ ; --lvl) {
+	rel_geom = mh_.GeometryInParent(lvl, *parent);
+	parent = mh_.ParentEntity(lvl, *parent);
+	local_parent = rel_geom->Global(local_parent);
+    }
     return mf_(*parent, local_parent);
   }
 
+  /**
+   * @brief Access the underlying Mesh
+   * @returns A shared pointer to the Mesh on which this MeshFunction is defined
+   */
   std::shared_ptr<const lf::mesh::Mesh> getMesh() const {
     return mh_.getMesh(level_coarse_ + 1);
   }
@@ -60,11 +97,12 @@ class MeshFunctionTransfer {
   const lf::refinement::MeshHierarchy &mh_;
   const MF &mf_;
   const lf::base::size_type level_coarse_;
+  const lf::base::size_type level_fine_;
 };
 
 template <typename MF>
 MeshFunctionTransfer(const lf::refinement::MeshHierarchy &, const MF &,
-                     lf::base::size_type)
+                     lf::base::size_type, lf::base::size_type)
     ->MeshFunctionTransfer<MF>;
 
 }  // namespace lf::refinement
