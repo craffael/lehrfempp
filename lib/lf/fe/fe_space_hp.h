09118a3ad7b75fa31f01e14f4c17c590c3719e1e
Tobias Rohner <torohner@student.ethz.ch>
2020-05-27 19:16:35 +0200

    Fix segfault

--- lib/lf/fe/fe_space_hp.h
+++ lib/lf/fe/fe_space_hp.h
@@ -34,8 +34,8 @@ class FeSpaceHP : public ScalarFESpace<SCALAR> {
    * @param mesh_p A shared pointer to the underlying mesh (immutable)
    * @param N The polynomial degree of the Finite Element Space
    */
-  explicit FeSpaceHP(std::shared_ptr<const lf::mesh::Mesh> mesh_p, unsigned N)
-      : ScalarFESpace<SCALAR>(std::move(mesh_p)),
+  explicit FeSpaceHP(const std::shared_ptr<const lf::mesh::Mesh>& mesh_p, unsigned N)
+      : ScalarFESpace<SCALAR>(mesh_p),
         ref_el_(mesh_p, nullptr),
         degree_(N) {
     init();
