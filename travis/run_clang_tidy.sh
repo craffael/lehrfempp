bbf2bc5607c503c0fa2ea58505c5403586b7667f
craffael <raffael@casagrande.ch>
2020-07-30 21:11:43 +0200

    run clang-tidy for projects separately.

--- travis/run_clang_tidy.sh
+++ travis/run_clang_tidy.sh
@@ -1,8 +1,9 @@
 # This script is called from .travis.yml
 #
-# It takes two inputs as environment variables:
+# It takes three inputs as environment variables:
 #   COMPILER specifies the compiler to use (e.g. g++-7)
 #   BUILD_TYPE specifies the CMAKE_BUILD_TYPE.
+#   FILES a regex which specifies the files that should be linted.
 # 
 # It builds and tests Lehrfempp using the provided compiler + cmake configuration.
 
@@ -17,4 +18,5 @@ source $(dirname $0)/build_dependencies.sh
 # run clang-tidy
 cd Build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On .
-../run_clang_tidy.sh
+# Run it except for projects:
+../run_clang_tidy.sh --file ${FILES}
