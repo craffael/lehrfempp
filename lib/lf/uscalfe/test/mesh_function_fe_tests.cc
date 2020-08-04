912b511170e7ddfe9cffe360c91e65955a79763a
ralfh <ralfh@ethz.ch>
2020-08-04 18:32:42 +0200

    Added comments and a test for cubic Lagrangian finite elements

--- lib/lf/uscalfe/test/mesh_function_fe_tests.cc
+++ lib/lf/uscalfe/test/mesh_function_fe_tests.cc
@@ -30,7 +30,7 @@ TEST(meshFunctionFE, Projection) {
 }
   
 TEST(meshFunctionFE, ProjectionQuad) {
-  // This test projects a linear mesh function onto a fe space and compares
+  // This test projects a quadratic mesh function onto a fe space and compares
   // the MeshFunctionFE with the original mesh function.
 
   auto mesh = lf::mesh::test_utils::GenerateHybrid2DTestMesh(0);
@@ -43,6 +43,22 @@ TEST(meshFunctionFE, ProjectionQuad) {
   auto projected = NodalProjection(*fespaceO2, mf_quad);
   auto mf_projected = MeshFunctionFE(fespaceO2, projected);
   mesh::utils::test::checkMeshFunctionEqual(*mesh, mf_quad, mf_projected);
+}
+  
+TEST(meshFunctionFE, ProjectionCubic) {
+  // This test projects a quadratic mesh function onto a fe space and compares
+  // the MeshFunctionFE with the original mesh function.
+
+  auto mesh = lf::mesh::test_utils::GenerateHybrid2DTestMesh(0);
+  // A cubic polynomial that can exactly be represented in the FE space
+  auto mf_cubic = mesh::utils::MeshFunctionGlobal(
+      [](const Eigen::Vector2d& x) { return x[0]*x[1]*x[1] + x[0]*x[0] + 2 * x[1]; });
+
+  auto fespaceO3 = std::make_shared<FeSpaceLagrangeO3<double>>(mesh);
+
+  auto projected = NodalProjection(*fespaceO3, mf_cubic);
+  auto mf_projected = MeshFunctionFE(fespaceO3, projected);
+  mesh::utils::test::checkMeshFunctionEqual(*mesh, mf_cubic, mf_projected);
 }
 
 }  // namespace lf::uscalfe::test
