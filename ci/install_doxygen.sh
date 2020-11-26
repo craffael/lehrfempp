0a0d615a815fafe6cd5e964f9715fa94760cbca5
craffael <raffael@casagrande.ch>
2020-11-26 21:16:19 +0100

    try doxygen 1.8.20

--- ci/install_doxygen.sh
+++ ci/install_doxygen.sh
@@ -8,15 +8,15 @@ dir=$(pwd)
 
 mkdir -p ${DEPS_DIR} && cd ${DEPS_DIR}
 
-if [ ! -d "doxygen-Release_1_8_17" ]; then
-  wget -O - https://github.com/doxygen/doxygen/archive/Release_1_8_17.tar.gz | tar xz
-  cd doxygen-Release_1_8_17
+if [ ! -d "doxygen-Release_1_8_20" ]; then
+  wget -O - https://github.com/doxygen/doxygen/archive/Release_1_8_20.tar.gz | tar xz
+  cd doxygen-Release_1_8_20
   mkdir build
   cd build
   cmake -Duse_libclang=ON  ..
   make -j2
 fi
-export PATH=${DEPS_DIR}/doxygen-Release_1_8_17/build/bin:$PATH
+export PATH=${DEPS_DIR}/doxygen-Release_1_8_20/build/bin:$PATH
 doxygen --version
 
 #Change back to where we left off.
