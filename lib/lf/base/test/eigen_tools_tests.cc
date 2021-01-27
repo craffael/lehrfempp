69f2f284b6337dee90cb491904385497d9e1dd65
craffael <raffael@casagrande.ch>
2021-01-27 15:22:36 +0100

    fix include order

--- lib/lf/base/test/eigen_tools_tests.cc
+++ lib/lf/base/test/eigen_tools_tests.cc
@@ -6,12 +6,10 @@
  * @copyright MIT License
  */
 
-// clang-format off
-#include <Eigen/Core>
-// clang-format on
-
 #include <gtest/gtest.h>
 #include <lf/base/base.h>
+
+#include <Eigen/Core>
 
 #include "lf/base/eigen_tools.h"
 
