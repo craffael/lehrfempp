664a51d8de1d8b27db5a32dc4eb82f14d554cd64
ralfh <ralfh@ethz.ch>
2021-07-13 20:16:30 +0200

    Edited some comments Fixed awkward local refinement rule in mesh_hierarchy.cc

--- lib/lf/refinement/test/father_child_relation_tests.cc
+++ lib/lf/refinement/test/father_child_relation_tests.cc
@@ -13,33 +13,34 @@ namespace lf::refinement::test {
 
 TEST(lf_refinement, FatherChildRelations) {
   MeshHierarchy::Logger()->set_level(spdlog::level::trace);
-
+  // Read test mesh
   auto gmsh_reader =
       io::test_utils::getGmshReader("two_element_hybrid_2d.msh", 2);
   auto base_mesh = gmsh_reader.mesh();
-
+  // Initialize mesh hierarchy with coarsest mesh 
   MeshHierarchy mh(base_mesh, std::make_unique<mesh::hybrid2d::MeshFactory>(2));
 
   // mark all edges of the triangle for refinement:
   auto marks = mesh::utils::make_CodimMeshDataSet(base_mesh, 1, false);
-  auto triangle = std::find_if(
+  const auto *triangle = std::find_if(
       base_mesh->Entities(0).begin(), base_mesh->Entities(0).end(),
       [](const auto e) { return e->RefEl() == base::RefEl::kTria(); });
-
-  for (auto edge : (*triangle)->SubEntities(1)) {
+  // Visit all edges of the triangle 
+  for (const auto *edge : (*triangle)->SubEntities(1)) {
     (*marks)(*edge) = true;
   }
-
-  mh.MarkEdges([&](auto &mesh, auto &e) { return (*marks)(e); });
+  // Mark all edges of the triangle 
+  mh.MarkEdges([&](auto &/*mesh*/, auto &e) { return (*marks)(e); });
+  // Invoke local refinement 
   mh.RefineMarked();
-
   {
-    // For debug purposes: print the meshs into a vtk file
+    // For debugging purposes: print the meshs into a vtk file
     io::VtkWriter writer0(base_mesh, "mesh0.vtk");
     io::VtkWriter writer1(mh.getMesh(1), "mesh1.vtk");
   }
-
+  // Obtain pointer to mesh created by local refinement
   auto child_mesh1 = mh.getMesh(1);
+  // Checks
   checkFatherChildRelations(mh, 0);
   checkGeometryInParent(mh, 0);
 }
--- lib/lf/refinement/test/run_rrt.sh
+++ lib/lf/refinement/test/run_rrt.sh
@@ -12,7 +12,7 @@ else
     PLOTLFMESHDIR=$(dirname $PLOTLFMESH);
     PLOTLFMESH=$(basename $PLOTLFMESH);
     PLOTMULTIREF=`find . -name "plot_multiref.m" -print | head -1`;
-    PLOTMULTIREFDIR="`dirname $PLOTMULTIREF`";
+    PLOTMULTIREFDIR=$(dirname $PLOTMULTIREF);
     PLOTMULTIREF=$(basename $PLOTMULTIREF);
     PLOTMULTIREF=${PLOTMULTIREF%.m};
     cd $TESTDIR || { echo "$TESTDIR inaccessible"; exit -1; }
@@ -24,6 +24,6 @@ else
     # Run the matlab postprocessing
     echo "<< Running matlab for postprocessing >>";
     # echo "$LFROOT/$PLOTLFMESHDIR, $LFROOT/$PLOTMULTIREFDIR";
-    echo "addpath('$LFROOT/$PLOTLFMESHDIR','$LFROOT/$PLOTMULTIREFDIR'); $PLOTMULTIREF; quit" > lfruntest.m
+    echo "addpath('$LFROOT/$PLOTLFMESHDIR','$LFROOT/$PLOTMULTIREFDIR','$LFROOT/$TESTDIR'); $PLOTMULTIREF; quit" > lfruntest.m
     matlab -nosplash -nodesktop -r "lfruntest";
 fi
