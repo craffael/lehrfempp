3a9f2e07e564f3be0b8930063b34dbed9242b15d
Nico Graf <ncograf@gmail.com>
2022-03-13 14:18:49 +0100

    Implement 3 sphere mesh

---
+++ projects/hldo_sphere/README.md
@@ -0,0 +1,3 @@
+# Hodge Laplacian and Dirac Operators on the 3-Sphere
+
+This folder contains the code developed in the context of the bachelor thesis *"Hodge Laplacian and Dirac Operators on the 3-Sphere"* by Nico Graf under the supervision of Prof. Dr. Ralf Hiptmair.
---
+++ projects/hldo_sphere/mesh/sphere_triag_mesh_builder.h
@@ -0,0 +1,85 @@
+#ifndef THESIS_MESH_SHPERE_TRIAG_MESH_BUILDER_H
+#define THESIS_MESH_SHPERE_TRIAG_MESH_BUILDER_H
+
+/**
+ * @file sphere_triag_mesh_builder.h
+ * @brief Constructs a mesh on an annulu
+ */
+
+#define _USE_MATH_DEFINES
+
+#include <lf/mesh/mesh.h>
+
+namespace projects::hldo_sphere {
+
+namespace mesh {
+
+/**
+ * @brief A mesh builder for regular 3-Sphere
+ *
+ * The diameter of the hole is controlled by the value passed to
+ * `setRadius(r)`.
+ * The refinement level can be set with `setRefinementLevel(n)`.
+ * The refinement level indicates by how many times the edges of the
+ * inital structure (icosahedron, see https://en.wikipedia.org/wiki/Icosahedron)
+ * will be regualarly split. Note that this is only conceptual, the
+ * implementation does not split egdes but directly build the desired sphere.
+ * The radius defaults to 1.0 while refinement level defaults to 0.
+ */
+class SphereTriagMeshBuilder {
+ public:
+  /**
+   * @brief Constructor
+   * @param mesh_factory A unique pointer to a mesh factory object used for the
+   * assembly of the mesh
+   */
+  explicit SphereTriagMeshBuilder(
+      std::unique_ptr<lf::mesh::MeshFactory> mesh_factory)
+      : radius_(1.0),
+        refinement_level_(0),
+        mesh_factory_(std::move(mesh_factory)) {
+    // Nothing to do here
+  }
+
+  /**
+   * @brief Sets the radius
+   * @param r The radius of the sphere
+   *
+   * requries r > 0
+   */
+  void setRadius(double r) {
+    LF_ASSERT_MSG(0 < r, "radius needs to be positive");
+    radius_ = r;
+  }
+
+  /**
+   * @brief Set the refinement level
+   * @param n The nummber of refinements
+   *
+   * The refinement level indicates by how many times the edges of the
+   * inital structure (icosahedron, see
+   * https://en.wikipedia.org/wiki/Icosahedron) will be split regualarly. Note
+   * that this is only conceptual, the implementation does not split egdes but
+   * directly build the desired sphere.
+   */
+  void setRefinementLevel(lf::base::size_type n) { refinement_level_ = n; }
+
+  /**
+   * @brief Build the mesh
+   * @returns A shared pointer to a mesh in the form of a sphere. The radius is
+   * set with `setRadius(r)` and the refinement level is set with
+   * `setRefinementLevel(n)`.
+   */
+  std::shared_ptr<lf::mesh::Mesh> Build();
+
+ private:
+  double radius_;
+  lf::base::size_type refinement_level_;
+  std::unique_ptr<lf::mesh::MeshFactory> mesh_factory_;
+};
+
+}  // end namespace mesh
+
+}  // namespace projects::hldo_sphere
+
+#endif  // THESIS_MESH_SPHERE_TRIAG_MESH_BUILDER_H
