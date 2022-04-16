e8004ebc028173ae51bf84707051cf09fe8156b8
Nico Graf <ncograf@gmail.com>
2022-04-16 20:21:08 +0200

    Update mesh example to output octacon infos

--- projects/hldo_sphere/examples/sphere_mesh/create_mesh.cc
+++ projects/hldo_sphere/examples/sphere_mesh/create_mesh.cc
@@ -13,6 +13,12 @@
  * 5
  * 6
  * 7
+ *
+ * And print information about the octacon
+ * For each cell
+ * local -> global mapping for edges
+ * edge orientations for all edges
+ *
  */
 int main(int arg, char** args) {
   // prepare needed objects
@@ -26,6 +32,31 @@ int main(int arg, char** args) {
 
   sphere.setRefinementLevel(0);
   const std::shared_ptr<lf::mesh::Mesh> mesh_0 = sphere.Build();
+
+  std::cout << "\n\n\nPoint information";
+  for (const lf::mesh::Entity* point : mesh_0->Entities(2)) {
+    Eigen::MatrixXd vertex = lf::geometry::Corners(*(point->Geometry()));
+    int index = mesh_0->Index(*point);
+    std::cout << "\n\nPoint " << index << "\n" << vertex;
+  }
+
+  for (const lf::mesh::Entity* cell_p : mesh_0->Entities(0)) {
+    int global_cell_index = mesh_0->Index(*cell_p);
+    auto orientations = cell_p->RelativeOrientations();
+    Eigen::MatrixXd vertices = lf::geometry::Corners(*(cell_p->Geometry()));
+    std::cout << "\n\n\nInfo for cell " << global_cell_index << "\n";
+    std::cout << "Corners:\n" << vertices;
+
+    int i = 0;
+    for (const lf::mesh::Entity* edge_p : cell_p->SubEntities(1)) {
+      int global_edge_index = mesh_0->Index(*edge_p);
+      int sign = lf::mesh::to_sign(orientations[i]);
+      std::cout << "\n\nEdge local " << i << " global " << global_edge_index
+                << " orientation " << sign;
+      i++;
+    }
+  }
+
   lf::io::VtkWriter vtk_writer_0(mesh_0, "sphere_0.vtk");
 
   sphere.setRefinementLevel(1);
@@ -44,15 +75,15 @@ int main(int arg, char** args) {
   const std::shared_ptr<lf::mesh::Mesh> mesh_4 = sphere.Build();
   lf::io::VtkWriter vtk_writer_4(mesh_4, "sphere_4.vtk");
 
-  sphere.setRefinementLevel(5);
-  const std::shared_ptr<lf::mesh::Mesh> mesh_5 = sphere.Build();
-  lf::io::VtkWriter vtk_writer_5(mesh_5, "sphere_5.vtk");
+  // sphere.setRefinementLevel(5);
+  // const std::shared_ptr<lf::mesh::Mesh> mesh_5 = sphere.Build();
+  // lf::io::VtkWriter vtk_writer_5(mesh_5, "sphere_5.vtk");
 
-  sphere.setRefinementLevel(6);
-  const std::shared_ptr<lf::mesh::Mesh> mesh_6 = sphere.Build();
-  lf::io::VtkWriter vtk_writer_6(mesh_6, "sphere_6.vtk");
+  // sphere.setRefinementLevel(6);
+  // const std::shared_ptr<lf::mesh::Mesh> mesh_6 = sphere.Build();
+  // lf::io::VtkWriter vtk_writer_6(mesh_6, "sphere_6.vtk");
 
-  sphere.setRefinementLevel(7);
-  const std::shared_ptr<lf::mesh::Mesh> mesh_7 = sphere.Build();
-  lf::io::VtkWriter vtk_writer_7(mesh_7, "sphere_7.vtk");
+  //  sphere.setRefinementLevel(7);
+  //  const std::shared_ptr<lf::mesh::Mesh> mesh_7 = sphere.Build();
+  //  lf::io::VtkWriter vtk_writer_7(mesh_7, "sphere_7.vtk");
 }
