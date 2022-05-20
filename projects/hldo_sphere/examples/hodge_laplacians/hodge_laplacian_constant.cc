c6441401dc414066e3efa89e12c03afd593ee322
Nico Graf <ncograf@gmail.com>
2022-05-20 08:52:17 +0200

    Add pretty prinitng for general solutions

--- projects/hldo_sphere/examples/hodge_laplacians/hodge_laplacian_constant.cc
+++ projects/hldo_sphere/examples/hodge_laplacians/hodge_laplacian_constant.cc
@@ -1,24 +1,19 @@
 /**
  * @file hodge_laplacian_constant.cc
- * @brief Solve hodge laplacian source problem for fixed function u = 5.2 and
- * fixed k = 100
+ * @brief Solve hodge laplacian source problem for fixed function u = 5.2
+ *
  */
 
 #include <hodge_laplacians_source_problems.h>
-#include <lf/io/vtk_writer.h>
 #include <lf/mesh/hybrid2d/mesh_factory.h>
 #include <lf/mesh/utils/tp_triag_mesh_builder.h>
 #include <lf/mesh/utils/utils.h>
 #include <lf/quad/quad.h>
-#include <lf/refinement/refinement.h>
-#include <lf/uscalfe/uscalfe.h>
-#include <mesh_function_whitney_one.h>
-#include <mesh_function_whitney_two.h>
-#include <mesh_function_whitney_zero.h>
 #include <norms.h>
+#include <results_processing.h>
 #include <sphere_triag_mesh_builder.h>
-#include <two_form_to_mesh_data_set.h>
 
+#include <chrono>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -28,42 +23,21 @@
 using lf::uscalfe::operator-;
 
 /**
- * @brief Concatenate objects defining an operator<<(std::ostream&)
- * @param args A variadic pack of objects implementing
- * `operator<<(std::ostream&)`
- * @returns A string with the objects concatenated
- */
-template <typename... Args>
-static std::string concat(Args &&...args) {
-  std::ostringstream ss;
-  (ss << ... << args);
-  return ss.str();
-}
-
-/**
- * @brief stores information to recover convergence properties
- */
-struct ProblemSolution {
-  std::shared_ptr<const lf::mesh::Mesh> mesh;
-  Eigen::VectorXd mu_zero;
-  Eigen::VectorXd mu_one;
-  Eigen::VectorXd mu_two;
-};
-
-/**
- * @brief Prints the L2 norm errors of the experiment
+ * @brief Prints the L2 norm errors and the Supremum of the experiment and
+ * creates vtk plots for a constant function u = 5.2
  */
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " max_refinement_level radius"
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0] << " max_refinement_level k radius"
               << std::endl;
     exit(1);
   }
 
-  const double k = 100;
+  double k;
   const unsigned refinement_level = atoi(argv[1]);
   double r;
-  sscanf(argv[2], "%lf", &r);
+  sscanf(argv[2], "%lf", &k);
+  sscanf(argv[3], "%lf", &r);
   std::cout << "max_refinement_level : " << refinement_level << std::endl;
 
   // Read the mesh from the gmsh file
@@ -76,11 +50,6 @@ int main(int argc, char *argv[]) {
   // we always take the same radius
   sphere.setRadius(r);
 
-  // mathematica function output requries the following helpers
-  auto Power = [](double a, double b) -> double { return std::pow(a, b); };
-  auto Sin = [](double a) -> double { return std::sin(a); };
-  auto Cos = [](double a) -> double { return std::cos(a); };
-
   // righthandside for the zero and two form
   auto f_zero = [&](const Eigen::Vector3d &x_vec) -> double {
     return k * k * 5.2;
@@ -91,45 +60,6 @@ int main(int argc, char *argv[]) {
     return Eigen::VectorXd::Constant(3, k * k * 5.2);
   };
 
-  // Solve the problem for each mesh in the hierarchy
-  std::vector<ProblemSolution> solutions(refinement_level + 1);
-
-  projects::hldo_sphere::discretization::HodgeLaplaciansSourceProblems
-      lse_builder;
-  lse_builder.SetLoadFunctions(f_zero, f_one, f_zero);
-
-  for (lf::base::size_type lvl = 0; lvl < refinement_level + 1; ++lvl) {
-    // get mesh
-    sphere.setRefinementLevel(lvl);
-    const std::shared_ptr<lf::mesh::Mesh> mesh = sphere.Build();
-
-    auto &sol = solutions[lvl];
-    sol.mesh = mesh;
-
-    // setup the problem
-    lse_builder.SetMesh(mesh);
-    lse_builder.SetK(k);
-
-    // compute the system
-    lse_builder.Compute();
-
-    // solve the system
-    lse_builder.Solve();
-
-    // store solutions
-    sol.mu_zero = lse_builder.GetMuZero();
-    sol.mu_one = std::get<0>(lse_builder.GetMuOne());
-    sol.mu_two = std::get<1>(lse_builder.GetMuTwo());
-
-    std::cout << "\nVector for two \n"
-              << sol.mu_two << "\n\nDivergence of two\n"
-              << std::get<0>(lse_builder.GetMuTwo()) << "\n\n"
-              << "\n\nMatrix of two\n"
-              << lse_builder.GetGalerkinMatrix(2).makeDense() << "\n\n"
-              << "\n\nPhi of two\n"
-              << lse_builder.GetLoadVector(2) << "\n\n";
-  }
-
   // Compute the analytic solution of the problem
   auto u_zero = [&](const Eigen::Vector3d x_vec) -> double { return 5.2; };
 
@@ -138,78 +68,99 @@ int main(int argc, char *argv[]) {
     return Eigen::VectorXd::Constant(3, 5.2);
   };
 
-  for (lf::base::size_type lvl = 0; lvl <= refinement_level; ++lvl) {
-    // create mesh Functions for solutions
+  // Solve the problem for each mesh in the hierarchy
+  std::vector<projects::hldo_sphere::post_processing::ProblemSolution>
+      solutions(refinement_level + 1);
+
+  projects::hldo_sphere::discretization::HodgeLaplaciansSourceProblems
+      lse_builder;
+  lse_builder.SetLoadFunctions(f_zero, f_one, f_zero);
+
+  // start timer for total time
+  std::chrono::steady_clock::time_point start_time_total =
+      std::chrono::steady_clock::now();
+
+  for (lf::base::size_type lvl = 0; lvl < refinement_level + 1; ++lvl) {
+    std::cout << "\nStart computation of refinement_level " << lvl << " "
+              << std::flush;
+
+    // start timer
+    std::chrono::steady_clock::time_point start_time =
+        std::chrono::steady_clock::now();
+
+    // get mesh
+    sphere.setRefinementLevel(lvl);
+    const std::shared_ptr<lf::mesh::Mesh> mesh = sphere.Build();
+
+    // end timer
+    std::chrono::steady_clock::time_point end_time =
+        std::chrono::steady_clock::now();
+    double elapsed_sec = std::chrono::duration_cast<std::chrono::milliseconds>(
+                             end_time - start_time)
+                             .count() /
+                         1000.;
+
+    std::cout << " -> Built Mesh " << elapsed_sec << " [s] " << std::flush;
+
     auto &sol = solutions[lvl];
-    projects::hldo_sphere::post_processing::MeshFunctionWhitneyZero mf_zero(
-        sol.mu_zero, sol.mesh);
-    projects::hldo_sphere::post_processing::MeshFunctionWhitneyOne mf_one(
-        sol.mu_one, sol.mesh);
-    projects::hldo_sphere::post_processing::MeshFunctionWhitneyTwo mf_two(
-        sol.mu_two, sol.mesh);
+    sol.mesh = mesh;
 
-    lf::mesh::utils::MeshFunctionGlobal<decltype(u_zero)> mf_zero_ana(u_zero);
+    // setup the problem
+    lse_builder.SetMesh(mesh);
+    lse_builder.SetK(k);
 
-    lf::mesh::utils::MeshFunctionGlobal<decltype(u_one)> mf_one_ana(u_one);
+    // start timer
+    start_time = std::chrono::steady_clock::now();
 
-    // Compute the error of the solutions
-    auto mf_diff_zero = mf_zero - mf_zero_ana;
-    auto mf_diff_one = mf_one - mf_one_ana;
-    auto mf_diff_two = mf_two - mf_zero_ana;
+    // compute the system
+    lse_builder.Compute();
 
-    // Perform post processing on the data
-    lf::io::VtkWriter writer(sol.mesh, concat("result_constant_", lvl, ".vtk"));
+    // end timer
+    end_time = std::chrono::steady_clock::now();
+    elapsed_sec = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      end_time - start_time)
+                      .count() /
+                  1000.;
 
-    // write the points out to the result vtk
-    // first get dataset
-    lf::mesh::utils::CodimMeshDataSet<double> data_set_solution_zero =
-        projects::hldo_sphere::post_processing::extractSolution(sol.mesh,
-                                                                sol.mu_two);
-    writer.WriteCellData(concat("mf_two_", lvl), data_set_solution_zero);
+    std::cout << " -> Computed LSE " << elapsed_sec << " [s] " << std::flush;
 
-    // writer.WriteCellData(concat("mf_diff_one_", lvl), mf_diff_one);
-    // writer.WriteCellData(concat("mf_diff_two_", lvl), mf_diff_two);
+    // start timer
+    start_time = std::chrono::steady_clock::now();
 
-    auto square_scalar = [](double a) -> double { return a * a; };
-    auto square_vector = [](Eigen::VectorXd a) -> double {
-      return a.squaredNorm();
-    };
+    // solve the system
+    lse_builder.Solve();
 
-    lf::quad::QuadRule qr =
-        lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 3);
+    // end timer
+    end_time = std::chrono::steady_clock::now();
 
-    // get error for zero form
-    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_zero =
-        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_zero,
-                                                       square_scalar, qr);
-    const double error_zero = std::get<0>(L2_zero);
-    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_zero =
-        std::get<1>(L2_zero);
-    writer.WriteCellData(concat("mf_zero_diff_", lvl), data_set_error_zero);
+    elapsed_sec = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      end_time - start_time)
+                      .count() /
+                  1000.;
 
-    // get error for one form
-    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_one =
-        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_one,
-                                                       square_vector, qr);
-    const double error_one = std::get<0>(L2_one);
-    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_one =
-        std::get<1>(L2_one);
-    writer.WriteCellData(concat("mf_one_diff_", lvl), data_set_error_one);
+    std::cout << " -> Solved System " << elapsed_sec << " [s] " << std::flush;
 
-    // get error for two form
-    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_two =
-        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_two,
-                                                       square_scalar, qr);
-    const double error_two = std::get<0>(L2_two);
-    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_two =
-        std::get<1>(L2_two);
-    writer.WriteCellData(concat("mf_two_diff_", lvl), data_set_error_two);
-
-    // print all the errors
-    std::cout << lvl << ' ' << solutions[lvl].mesh->NumEntities(2) << ' '
-              << error_zero << ' ' << error_one << ' ' << error_two
-              << std::endl;
+    // store solutions
+    sol.mu_zero = lse_builder.GetMuZero();
+    sol.mu_one = std::get<0>(lse_builder.GetMuOne());
+    sol.mu_two = std::get<1>(lse_builder.GetMuTwo());
   }
+
+  // end timer total
+  std::chrono::steady_clock::time_point end_time_total =
+      std::chrono::steady_clock::now();
+  double elapsed_sec_total =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end_time_total -
+                                                            start_time_total)
+          .count() /
+      1000.;
+
+  std::cout << "\nTotal computation time for all levels " << elapsed_sec_total
+            << " [s]\n";
+
+  projects::hldo_sphere::post_processing::process_results<
+      decltype(u_zero), decltype(u_one), decltype(u_zero)>(
+      "constant", solutions, u_zero, u_one, u_zero);
 
   return 0;
 }
---
+++ projects/hldo_sphere/post_processing/results_processing.h
@@ -0,0 +1,211 @@
+#ifndef THESIS_POST_PROCESSING_RESULTS_PROCESSING_H
+#define THESIS_POST_PROCESSING_RESULTS_PROCESSING_H
+
+/**
+ * @file result_processing
+ *
+ * @brief takes a list of results and creates vtk files, plots and tables
+ */
+
+#include <lf/io/vtk_writer.h>
+#include <mesh_function_whitney_one.h>
+#include <mesh_function_whitney_two.h>
+#include <mesh_function_whitney_zero.h>
+#include <norms.h>
+#include <sphere_triag_mesh_builder.h>
+#include <two_form_to_mesh_data_set.h>
+
+#include <Eigen/Dense>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace projects::hldo_sphere {
+
+namespace post_processing {
+
+// operator used to subract meshfunctions
+using lf::uscalfe::operator-;
+
+/**
+ * @brief stores information to recover convergence properties
+ *
+ * @tparam U_ZERO functor for the analytical solution of the zero form
+ * @tparam U_ONE functor for the analytical solution of the one form
+ * @tparam U_TWO functor for the analytical solution of the two form
+ */
+struct ProblemSolution {
+  std::shared_ptr<const lf::mesh::Mesh> mesh;
+  Eigen::VectorXd mu_zero;
+  Eigen::VectorXd mu_one;
+  Eigen::VectorXd mu_two;
+};
+
+/**
+ * @brief Concatenate objects defining an operator<<(std::ostream&)
+ * @param args A variadic pack of objects implementing
+ * `operator<<(std::ostream&)`
+ * @returns A string with the objects concatenated
+ */
+template <typename... Args>
+static std::string concat(Args &&...args) {
+  std::ostringstream ss;
+  (ss << ... << args);
+  return ss.str();
+}
+
+/**
+ * @brief Generate vtk files containing meshdata, plots and tables with results
+ *
+ * @tparam U_ZERO functor for the analytical solution of the zero form
+ * @tparam U_ONE functor for the analytical solution of the one form
+ * @tparam U_TWO functor for the analytical solution of the two form
+ *
+ * @param name identifying the experiment (used to name the outputs)
+ * @param results contains a list of ProblemSolution structs, each containing a
+ * mesh with analyitical solutions and the coefficient vectors for the
+ * approximate solution
+ *
+ */
+template <typename U_ZERO, typename U_ONE, typename U_TWO>
+void process_results(std::string name, std::vector<ProblemSolution> &results,
+                     U_ZERO &u_zero, U_ONE u_one, U_TWO u_two) {
+  int n = results.size();
+
+  // prepare output
+  int table_width = 15;
+  int precision = 4;
+
+  // print all the errors
+  std::cout << std::endl
+            << std::endl
+            << std::endl
+            << std::setw(table_width) << "Norm Type" << std::setw(table_width)
+            << "Ref. level" << std::setw(table_width) << "Num Cells"
+            << std::setw(table_width) << "Error Zero" << std::setw(table_width)
+            << "Error One" << std::setw(table_width) << "Error Two" << std::endl
+            << std::endl;
+
+  // loop over all levels contained in the solution
+  for (lf::base::size_type lvl = 0; lvl < n; ++lvl) {
+    // create mesh Functions for solutions
+    auto &sol = results[lvl];
+    projects::hldo_sphere::post_processing::MeshFunctionWhitneyZero mf_zero(
+        sol.mu_zero, sol.mesh);
+    projects::hldo_sphere::post_processing::MeshFunctionWhitneyOne mf_one(
+        sol.mu_one, sol.mesh);
+    projects::hldo_sphere::post_processing::MeshFunctionWhitneyTwo mf_two(
+        sol.mu_two, sol.mesh);
+
+    lf::mesh::utils::MeshFunctionGlobal<U_ZERO> mf_zero_ana(u_zero);
+    lf::mesh::utils::MeshFunctionGlobal<U_ONE> mf_one_ana(u_one);
+    lf::mesh::utils::MeshFunctionGlobal<U_TWO> mf_two_ana(u_two);
+
+    // Compute the error of the solutions
+    auto mf_diff_zero = mf_zero - mf_zero_ana;
+    auto mf_diff_one = mf_one - mf_one_ana;
+    auto mf_diff_two = mf_two - mf_zero_ana;
+
+    // Perform post processing on the data
+    lf::io::VtkWriter writer(sol.mesh,
+                             concat("result_", name, "_", lvl, ".vtk"));
+
+    // write the points out to the result vtk
+    // first get dataset
+    lf::mesh::utils::CodimMeshDataSet<double> data_set_solution_zero =
+        projects::hldo_sphere::post_processing::extractTwoFormSolution(
+            sol.mesh, sol.mu_two);
+    writer.WriteCellData(concat("mf_two_", lvl), data_set_solution_zero);
+
+    // define square functions
+    auto square_scalar = [](double a) -> double { return a * a; };
+    auto square_vector = [](Eigen::VectorXd a) -> double {
+      return a.squaredNorm();
+    };
+
+    // define quadrule for norms
+    lf::quad::QuadRule qr =
+        lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 2);
+
+    // get error for zero form
+    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_zero =
+        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_zero,
+                                                       square_scalar, qr);
+    const double error_zero = std::get<0>(L2_zero);
+    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_zero =
+        std::get<1>(L2_zero);
+    writer.WriteCellData(concat("mf_zero_diff_", lvl), data_set_error_zero);
+
+    // get error for one form
+    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_one =
+        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_one,
+                                                       square_vector, qr);
+    const double error_one = std::get<0>(L2_one);
+    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_one =
+        std::get<1>(L2_one);
+    writer.WriteCellData(concat("mf_one_diff_", lvl), data_set_error_one);
+
+    // get error for two form
+    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_two =
+        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_two,
+                                                       square_scalar, qr);
+    const double error_two = std::get<0>(L2_two);
+    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_two =
+        std::get<1>(L2_two);
+    writer.WriteCellData(concat("mf_two_diff_", lvl), data_set_error_two);
+
+    // get sup error for zero form
+    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>>
+        sup_zero = projects::hldo_sphere::post_processing::SupNorm(
+            sol.mesh, mf_diff_zero, square_scalar, qr);
+    const double error_sup_zero = std::get<0>(sup_zero);
+    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_sup_zero =
+        std::get<1>(sup_zero);
+    writer.WriteCellData(concat("mf_zero_diff_sup_", lvl),
+                         data_set_error_sup_zero);
+
+    // get error for one form
+    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> sup_one =
+        projects::hldo_sphere::post_processing::SupNorm(sol.mesh, mf_diff_one,
+                                                        square_vector, qr);
+    const double error_sup_one = std::get<0>(sup_one);
+    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_sup_one =
+        std::get<1>(sup_one);
+    writer.WriteCellData(concat("mf_one_diff_sup_", lvl),
+                         data_set_error_sup_one);
+
+    // get error for two form
+    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> sup_two =
+        projects::hldo_sphere::post_processing::SupNorm(sol.mesh, mf_diff_two,
+                                                        square_scalar, qr);
+    const double error_sup_two = std::get<0>(sup_two);
+    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_sup_two =
+        std::get<1>(sup_two);
+    writer.WriteCellData(concat("mf_two_diff_sup_", lvl),
+                         data_set_error_sup_two);
+
+    // print all the L2 errors
+    std::cout << std::setprecision(precision) << std::setw(table_width)
+              << "L2_norm^2" << std::setw(table_width) << lvl
+              << std::setw(table_width) << sol.mesh->NumEntities(0)
+              << std::setw(table_width) << error_zero << std::setw(table_width)
+              << error_one << std::setw(table_width) << error_two << std::endl;
+
+    // print all the sup errors
+    std::cout << std::setw(table_width) << "sup_norm^2"
+              << std::setw(table_width) << lvl << std::setw(table_width)
+              << sol.mesh->NumEntities(0) << std::setw(table_width)
+              << error_sup_zero << std::setw(table_width) << error_sup_one
+              << std::setw(table_width) << error_sup_two << std::endl
+              << std::endl;
+  }
+}
+
+}  // end namespace post_processing
+
+}  // namespace projects::hldo_sphere
+
+#endif  // THESIS_POST_PROCESSING_RESULTS_PROCESSING_H
