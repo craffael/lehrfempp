ad2cb0c5b9f89052a21fbe32b0c7aa6687f7d12b
Nico Graf <ncograf@gmail.com>
2022-05-21 03:00:45 +0200

    Update hodge laplacians to be more modular

--- projects/hldo_sphere/examples/hodge_laplacians/hodge_laplacians.cc
+++ projects/hldo_sphere/examples/hodge_laplacians/hodge_laplacians.cc
@@ -16,6 +16,7 @@
 #include <mesh_function_whitney_two.h>
 #include <mesh_function_whitney_zero.h>
 #include <norms.h>
+#include <results_processing.h>
 #include <sphere_triag_mesh_builder.h>
 
 #include <cstdlib>
@@ -27,32 +28,8 @@
 using lf::uscalfe::operator-;
 
 /**
- * @brief Concatenate objects defining an operator<<(std::ostream&)
- * @param args A variadic pack of objects implementing
- * `operator<<(std::ostream&)`
- * @returns A string with the objects concatenated
- */
-template <typename... Args>
-static std::string concat(Args &&...args) {
-  std::ostringstream ss;
-  (ss << ... << args);
-  return ss.str();
-}
-
-/**
- * @brief stores information to recover convergence properties
- */
-struct ProblemSolution {
-  std::shared_ptr<const lf::mesh::Mesh> mesh;
-  Eigen::VectorXd mu_zero;
-  Eigen::VectorXd mu_one;
-  Eigen::VectorXd mu_two;
-};
-
-/**
  * @brief Prints the L2 norm errors and the Supremum of the experiment and
- * creates vtk plots
- *
+ * creates vtk plots for a function u
  */
 int main(int argc, char *argv[]) {
   if (argc != 4) {
@@ -138,37 +115,6 @@ int main(int argc, char *argv[]) {
     return ret;
   };
 
-  // Solve the problem for each mesh in the hierarchy
-  std::vector<ProblemSolution> solutions(refinement_level + 1);
-
-  projects::hldo_sphere::discretization::HodgeLaplaciansSourceProblems
-      lse_builder;
-  lse_builder.SetLoadFunctions(f_zero, f_one, f_zero);
-
-  for (lf::base::size_type lvl = 0; lvl < refinement_level + 1; ++lvl) {
-    // get mesh
-    sphere.setRefinementLevel(lvl);
-    const std::shared_ptr<lf::mesh::Mesh> mesh = sphere.Build();
-
-    auto &sol = solutions[lvl];
-    sol.mesh = mesh;
-
-    // setup the problem
-    lse_builder.SetMesh(mesh);
-    lse_builder.SetK(k);
-
-    // compute the system
-    lse_builder.Compute();
-
-    // solve the system
-    lse_builder.Solve();
-
-    // store solutions
-    sol.mu_zero = lse_builder.GetMuZero();
-    sol.mu_one = std::get<0>(lse_builder.GetMuOne());
-    sol.mu_two = std::get<1>(lse_builder.GetMuTwo());
-  }
-
   // Compute the analytic solution of the problem
   auto u_zero = [&](const Eigen::Vector3d x_vec) -> double {
     // first scale to the circle
@@ -197,72 +143,99 @@ int main(int argc, char *argv[]) {
     return ret;
   };
 
-  // Perform post processing on the data
-  lf::io::VtkWriter writer(solutions.back().mesh,
-                           "result."
-                           "vtk");
-  for (lf::base::size_type lvl = 0; lvl <= refinement_level; ++lvl) {
-    // create mesh Functions for solutions
+  // Solve the problem for each mesh in the hierarchy
+  std::vector<projects::hldo_sphere::post_processing::ProblemSolution>
+      solutions(refinement_level + 1);
+
+  projects::hldo_sphere::discretization::HodgeLaplaciansSourceProblems
+      lse_builder;
+  lse_builder.SetLoadFunctions(f_zero, f_one, f_zero);
+
+  // start timer for total time
+  std::chrono::steady_clock::time_point start_time_total =
+      std::chrono::steady_clock::now();
+
+  for (lf::base::size_type lvl = 0; lvl < refinement_level + 1; ++lvl) {
+    std::cout << "\nStart computation of refinement_level " << lvl << " "
+              << std::flush;
+
+    // start timer
+    std::chrono::steady_clock::time_point start_time =
+        std::chrono::steady_clock::now();
+
+    // get mesh
+    sphere.setRefinementLevel(lvl);
+    const std::shared_ptr<lf::mesh::Mesh> mesh = sphere.Build();
+
+    // end timer
+    std::chrono::steady_clock::time_point end_time =
+        std::chrono::steady_clock::now();
+    double elapsed_sec = std::chrono::duration_cast<std::chrono::milliseconds>(
+                             end_time - start_time)
+                             .count() /
+                         1000.;
+
+    std::cout << " -> Built Mesh " << elapsed_sec << " [s] " << std::flush;
+
     auto &sol = solutions[lvl];
-    projects::hldo_sphere::post_processing::MeshFunctionWhitneyZero mf_zero(
-        sol.mu_zero, sol.mesh);
-    projects::hldo_sphere::post_processing::MeshFunctionWhitneyOne mf_one(
-        sol.mu_one, sol.mesh);
-    projects::hldo_sphere::post_processing::MeshFunctionWhitneyTwo mf_two(
-        sol.mu_two, sol.mesh);
+    sol.mesh = mesh;
 
-    lf::mesh::utils::MeshFunctionGlobal<decltype(u_zero)> mf_zero_ana(u_zero);
+    // setup the problem
+    lse_builder.SetMesh(mesh);
+    lse_builder.SetK(k);
 
-    lf::mesh::utils::MeshFunctionGlobal<decltype(u_one)> mf_one_ana(u_one);
+    // start timer
+    start_time = std::chrono::steady_clock::now();
 
-    // Compute the error of the solutions
-    auto mf_diff_zero = mf_zero - mf_zero_ana;
-    auto mf_diff_one = mf_one - mf_one_ana;
-    auto mf_diff_two = mf_two - mf_zero_ana;
+    // compute the system
+    lse_builder.Compute();
 
-    auto square_scalar = [](double a) -> double { return a * a; };
-    auto square_vector = [](Eigen::VectorXd a) -> double {
-      return a.squaredNorm();
-    };
+    // end timer
+    end_time = std::chrono::steady_clock::now();
+    elapsed_sec = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      end_time - start_time)
+                      .count() /
+                  1000.;
 
-    lf::quad::QuadRule qr =
-        lf::quad::make_QuadRule(lf::base::RefEl::kTria(), 3);
+    std::cout << " -> Computed LSE " << elapsed_sec << " [s] " << std::flush;
 
-    // Perform post processing on the data
-    lf::io::VtkWriter writer(sol.mesh, concat("result_test_", lvl, ".vtk"));
+    // start timer
+    start_time = std::chrono::steady_clock::now();
 
-    // get error for zero form
-    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_zero =
-        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_zero,
-                                                       square_scalar, qr);
-    const double error_zero = std::get<0>(L2_zero);
-    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_zero =
-        std::get<1>(L2_zero);
-    writer.WriteCellData(concat("mf_zero_diff_", lvl), data_set_error_zero);
+    // solve the system
+    lse_builder.Solve();
 
-    // get error for one form
-    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_one =
-        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_one,
-                                                       square_vector, qr);
-    const double error_one = std::get<0>(L2_one);
-    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_one =
-        std::get<1>(L2_one);
-    writer.WriteCellData(concat("mf_one_diff_", lvl), data_set_error_one);
+    // end timer
+    end_time = std::chrono::steady_clock::now();
 
-    // get error for two form
-    const std::pair<double, lf::mesh::utils::CodimMeshDataSet<double>> L2_two =
-        projects::hldo_sphere::post_processing::L2norm(sol.mesh, mf_diff_two,
-                                                       square_scalar, qr);
-    const double error_two = std::get<0>(L2_two);
-    const lf::mesh::utils::CodimMeshDataSet<double> data_set_error_two =
-        std::get<1>(L2_two);
-    writer.WriteCellData(concat("mf_two_diff_", lvl), data_set_error_two);
+    elapsed_sec = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      end_time - start_time)
+                      .count() /
+                  1000.;
 
-    // print all the errors
-    std::cout << lvl << ' ' << solutions[lvl].mesh->NumEntities(2) << ' '
-              << error_zero << ' ' << error_one << ' ' << error_two
-              << std::endl;
+    std::cout << " -> Solved System " << elapsed_sec << " [s] " << std::flush;
+
+    // store solutions
+    sol.mu_zero = lse_builder.GetMuZero();
+    sol.mu_one = std::get<0>(lse_builder.GetMuOne());
+    sol.mu_two = std::get<1>(lse_builder.GetMuTwo());
   }
+
+  // end timer total
+  std::chrono::steady_clock::time_point end_time_total =
+      std::chrono::steady_clock::now();
+  double elapsed_sec_total =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end_time_total -
+                                                            start_time_total)
+          .count() /
+      1000.;
+
+  std::cout << "\nTotal computation time for all levels " << elapsed_sec_total
+            << " [s]\n";
+
+  projects::hldo_sphere::post_processing::process_results<
+      decltype(u_zero), decltype(u_one), decltype(u_zero)>(
+      "test", solutions, u_zero, u_one, u_zero);
 
   return 0;
 }
