d48797f297d0ea9f74d081654370bcd96edab086
Nico Graf <ncograf@gmail.com>
2022-06-28 10:46:39 +0200

    Change plotscript and move testfunction file

---
+++ projects/hldo_sphere/post_processing/plotlaplace.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import click
+import pandas as pd 
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+@click.command()
+@click.option('-f', '--file', required=True, help='csv file containing the results to plot')
+
+def plot(file):
+
+    plt.rcParams.update({
+        "text.usetex": True,
+        "font.family" : 'Palatino', 
+        "font.size" : 11,
+    })
+
+    df = pd.read_csv(file)
+
+    # exclude supremum norm and meta data
+    df_y = df.iloc[1:,10:]
+
+    # get h values
+    df_h = df[["hMax"]].iloc[1:]
+    x = df_h.to_numpy().flatten()
+    df_x_label = "h"
+
+    ratezero = np.round(np.mean(df_y.iloc[1:,1])) # rounds to the 
+    rateone = np.round(np.mean(df_y.iloc[1:,3])) # rounds to the 
+    ratetwo = np.round(np.mean(df_y.iloc[1:,5])) # rounds to the 
+
+    # print table that is, print all l2 errors
+    print(df_y)
+
+    fig, axs = plt.subplots(1,1)
+
+    # rounds the axis on two siginficant positions
+    axs.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    axs.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+    # add the three lines
+    yzero = df_y.iloc[:, 0].to_numpy()
+    yone = df_y.iloc[:, 2].to_numpy()
+    ytwo = df_y.iloc[:, 4].to_numpy()
+    axs.loglog(x,yzero)
+    axs.loglog(x,yone)
+    axs.loglog(x,ytwo)
+
+    inter = yzero[0]
+    yref = x * inter * inter
+    axs.loglog(x,yref)
+    # set labels and swap the x direction to get decreasing error
+    axs.set(ylabel="$L^2$ error", xlabel=df_x_label, xlim=(max(x), min(x)))
+    axs.legend(["zero form", "one form", "two form", "err = h"])
+
+#    plt.title("$L^2$-Error of Hodge Laplacian Source Problems")
+    plt.savefig("hodgelaplacians.png", dpi=300)
+    plt.show()
+
+if __name__ == '__main__':
+    plot()
