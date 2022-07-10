667a1fb427f0ff18286d4d53e83f24d110f7c199
Nico Graf <ncograf@gmail.com>
2022-07-10 16:17:00 +0200

    Updtate Doc

--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -6,6 +6,5 @@ var searchData=
   ['convectionelementmatrixprovider_3',['ConvectionElementMatrixProvider',['../classprojects_1_1dpg_1_1_convection_element_matrix_provider.html',1,'projects::dpg']]],
   ['coomatrix_4',['COOMatrix',['../classlf_1_1assemble_1_1_c_o_o_matrix.html',1,'lf::assemble']]],
   ['coomatrix_3c_20complex_20_3e_5',['COOMatrix&lt; complex &gt;',['../classlf_1_1assemble_1_1_c_o_o_matrix.html',1,'lf::assemble']]],
-  ['coomatrix_3c_20double_20_3e_6',['COOMatrix&lt; double &gt;',['../classlf_1_1assemble_1_1_c_o_o_matrix.html',1,'lf::assemble']]],
-  ['cpu_5ftimes_7',['cpu_times',['../structlf_1_1base_1_1_timer_1_1cpu__times.html',1,'lf::base::Timer']]]
+  ['cpu_5ftimes_6',['cpu_times',['../structlf_1_1base_1_1_timer_1_1cpu__times.html',1,'lf::base::Timer']]]
 ];
