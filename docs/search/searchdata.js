624be998f92ea184dd8863d6b81973db2e9ef0b6
Nico Graf <ncograf@gmail.com>
2022-09-14 17:49:57 +0200

    Updtate docs on Nico Grafs github

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,45 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghijklmnopqrstuvw~",
-  1: "abcdefghilmnpqrstuvw",
-  2: "flnp",
-  3: "abcdefghijklmnopqrstuvw~",
-  4: "abcdefghijklmnopqrstuvw",
-  5: "acdefgilmnpqrstv",
-  6: "cefort",
-  7: "abcehknpqrtvw",
-  8: "mo",
-  9: "cems",
-  10: "eglmw"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "functions",
-  4: "variables",
-  5: "typedefs",
-  6: "enums",
-  7: "enumvalues",
-  8: "related",
-  9: "groups",
-  10: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Functions",
-  4: "Variables",
-  5: "Typedefs",
-  6: "Enumerations",
-  7: "Enumerator",
-  8: "Friends",
-  9: "Modules",
-  10: "Pages"
 };
 
