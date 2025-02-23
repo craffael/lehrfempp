69c1968c32790250e1e0c513e2aa0dc0c9cd5a4c
Raffael Casagrande <raffael@casagrande.ch>
2025-02-23 21:17:25 +0100

    add fix only for apple.

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,6 @@
 # This file specifies additional data for the dependencies that are imported via hunter
 
-
+if(APPLE)
 hunter_config(Boost 
   VERSION 1.86.0
   URL "https://archives.boost.io/release/1.86.0/source/boost_1_86_0.tar.bz2"
@@ -8,6 +8,14 @@ hunter_config(Boost
   fd0d26a7d5eadf454896942124544120e3b7a38f
   CMAKE_ARGS CMAKE_CXX_FLAGS=-std=c++20 # Required for OSX, otherwise it compiles with C++98
 )
+else()
+hunter_config(Boost 
+  VERSION 1.86.0
+  URL "https://archives.boost.io/release/1.86.0/source/boost_1_86_0.tar.bz2"
+  SHA1
+  fd0d26a7d5eadf454896942124544120e3b7a38f
+)
+endif()
 
 hunter_config(Eigen VERSION 3.4.0)
 hunter_config(GTest VERSION 1.11.0)
